9d4129701716fa7ff9d2a600e3dd20a7b871bebc
fjod <fourjustoneday@yandex.ru>
2020-05-12 13:32:18 +0300

    phoneNumber

---
+++ CodeWars/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CodeWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var phoneTest = PhoneNumber.Create(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0});
+            Console.WriteLine(phoneTest);
+            Console.ReadKey();
+        }
+    }
+}
