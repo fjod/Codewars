22680092375111ceae32a50c11f62c2cbbe6e2e1
fjod <fourjustoneday@Yandex.ru>
2021-01-08 11:11:12 +0300

    7 kyu Sum of odd numbers

--- FSharpKatas/Program.fs
+++ FSharpKatas/Program.fs
@@ -1,28 +1,34 @@
 ﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
-open System  
-let kataFunc (step:int) (lowerBound:int) (upperBound:int) =
-    let isPrime n =
-      let sqrt' = (float >> sqrt >> int) n // square root of integer
-      [ 2 .. sqrt' ] // all numbers from 2 to sqrt'
-      |> List.forall (fun x -> n % x <> 0) // no divisors
-    let ifStepIsEqual (a:int) = a.Equals(step)
-    let getStepForTwoPrimes ((a:int),(b:int)) = Math.Abs(a-b) |> ifStepIsEqual   
+open System
+
+//I misunderstood the task; this is written to calc sum for all tree; while we need the sum for only one row :/
+let kataFunc (n:int) =
+    let mutable sum = 0
+    let mutable prevEnd = 0
+    let getDigitsListForRow (row:int) (start:int) =
+        match row with 
+            | 1 -> [1]
+            | _ -> [start..2..start+2*(row-1)]
+            
+    let calcAndSave (input:int) : unit =       
+        if input.Equals 1 then
+            sum <- 1
+            prevEnd <- 1
+        else
+           let thisStart = prevEnd + 2
+           let prevList = getDigitsListForRow input thisStart
+           prevEnd <- List.last prevList
+           sum <- List.sum prevList
+           ()      
+       
+    [1..n] |> List.iter  calcAndSave
+    sum
     
-    let primes = [lowerBound..upperBound] |> List.filter isPrime |> List.pairwise 
-    let firstPair = List.find getStepForTwoPrimes primes
-    let a = fst firstPair
-    let b = snd firstPair    
-    [Int64.Parse(a.ToString());Int64.Parse(b.ToString())]
-    ///home/codewarrior/program.fsx(37,25): error FS0001: This expression was expected to have type
-    //'int'     
-    //but here has type
-    //  'int64'
-    // I was not able to pass test on codewars site because of this error^. 
+
 [<EntryPoint>]
 let main argv =
-    let checkNumberForPrimeAndPrint (list: int64 list) =
-        printf "First pair is -> %i %i" list.[0] list.[1]      
-    kataFunc 4 130 200 |>    checkNumberForPrimeAndPrint  
    
-    0 // return an integer exit code
+   kataFunc 3 |> printfn "%i" 
+   
+   0 // return an integer exit code
