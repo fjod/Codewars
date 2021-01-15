5be578447cdcbc8bc57be0a532e4ab74e11886ee
fjod <fourjustoneday@yandex.ru>
2021-01-15 14:30:01 +0300

    railway

--- FSharpKatas/Program.fs
+++ FSharpKatas/Program.fs
@@ -10,12 +10,30 @@ let inline findOdd (numbers: int list) =
            |> Seq.find (fun m -> isOdd (snd m))
            |> fst
  
+type Request = {name:string; email:string}
+
+let (>>=) twoTrackInput switchFunction = 
+    Option.bind switchFunction twoTrackInput 
+let validate1 input =
+   if input.name = "" then None  else Some input
+let validate2 input =
+   if input.name.Length > 50 then None else Some input
+let validate3 input =
+   if input.email = "" then None  else Some input        
+let combinedValidation x =
+    // connect the two-tracks together   
+   x |> validate1 >>= validate2  >>=  validate3
+   
+//https://fsharpforfunandprofit.com/posts/recipe-part2/
     
 [<EntryPoint>]
 let main argv =
-  
-
-   [20; 1; -1; 2; -2; 3; 3; 5; 5; 1; 2; 4; 20; 4; -1; -2; 5] |> findOdd |> printfn "%i"
+   let testRequest = {name = "QQseg"; email = " "}
+   combinedValidation testRequest |> ignore   
+   
+   let test = Some 1 |> Option.map (fun i-> i.ToString())
+   let test2 = Some 1 |> Option.bind (fun i-> Some (i.ToString()))
+   test.Value.Equals test2.Value |> printfn "%b" //true
    
    0 // return an integer exit code
    
