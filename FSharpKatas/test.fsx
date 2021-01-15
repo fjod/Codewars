90109efef0ec3e36b8af59d4dd4992b9b8193099
fjod <fourjustoneday@yandex.ru>
2021-01-15 12:54:19 +0300

    example script

---
+++ FSharpKatas/test.fsx
@@ -0,0 +1,29 @@
+﻿#r "nuget: FSharp.Data"
+open FSharp.Data
+//https://fsharpforfunandprofit.com/posts/low-risk-ways-to-use-fsharp-at-work-2/
+let queryServer uri queryParams = 
+    try
+        let response = Http.Request(uri, query=queryParams, silentHttpErrors = true)                
+        Some response 
+    with
+    | :? System.Net.WebException as ex -> None
+
+let sendAlert uri message = 
+    // send alert via email, say
+    printfn "Error for %s. Message=%O" uri message
+
+let checkServer (uri,queryParams) = 
+    match queryServer uri queryParams with
+    | Some response -> 
+        printfn "Response for %s is %O" uri response.StatusCode 
+        if (response.StatusCode <> 200) then
+            sendAlert uri response.StatusCode 
+    | None -> 
+        sendAlert uri "No response"
+
+// test the sites 
+let google = "http://google.com", ["q","fsharp"]
+let bad = "http://example.bad", []
+
+[google;bad]
+|> List.iter checkServer 
