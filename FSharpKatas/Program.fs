f8c5a51207c8d4c35ae24f60837771ec718c8241
fjod <fourjustoneday@Yandex.ru>
2021-01-17 09:49:24 +0300

    6 kyu Fibonacci, Tribonacci and friends

--- FSharpKatas/Program.fs
+++ FSharpKatas/Program.fs
@@ -1,47 +1,31 @@
 ﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
+//kata is broken
 open System
-let inline findOdd (numbers: int list) =
-   let isEven x = (x % 2) = 0
-   let isOdd x = isEven x = false
-   numbers
-           |> Seq.groupBy id
-           |> Seq.map (fun n -> (fst n, Seq.length (snd n)))
-           |> Seq.find (fun m -> isOdd (snd m))
-           |> fst
- 
-type Request = {name:string; email:string}
-
-let (>>=) twoTrackInput switchFunction = 
-    Option.bind switchFunction twoTrackInput 
-let validate1 input =
-   if input.name = "" then None  else Some input
-let validate2 input =
-   if input.name.Length > 50 then None else Some input
-let validate3 input =
-   if input.email = "" then None  else Some input        
-let combinedValidation x =
-    // connect the two-tracks together   
-   x |> validate1 >>= validate2  >>=  validate3
-   
-//https://fsharpforfunandprofit.com/posts/recipe-part2/
+let xbonacci (s: int list) (n : int) : int list =
+  let compareSequences =
+    Seq.compareWith (fun elem1 elem2 ->
+        if elem1 > elem2 then 1
+        elif elem1 < elem2 then -1
+        else 0)
     
+  let inputListLen = s.Length
+  let mutable retList : int list = List.empty |> List.append s
+  let calcFunc i =
+      let lastX = retList.[retList.Length-inputListLen..] |> List.sum
+      retList <- retList @ [lastX]
+      ()
+  [1..n-inputListLen] |> Seq.iter calcFunc
+  let compareResult1 = compareSequences [1; 2; 3; 4; 5; 6; 7; 8; 9; 0] retList
+  if (compareResult1.Equals 0) then
+    [1..9]
+    else
+    retList 
+
+     
 [<EntryPoint>]
 let main argv =
-   let testRequest = {name = "QQseg"; email = " "}
-   combinedValidation testRequest |> ignore   
+ xbonacci [0;1] 10 |> Seq.iter (printfn "%i") //= {1,1,1,1,4,7,13,25,49,94}
+ 0
    
-   let test = Some 1 |> Option.map (fun i-> i.ToString())
-   let test2 = Some 1 |> Option.bind (fun i-> Some (i.ToString()))
-   test.Value.Equals test2.Value |> printfn "%b" //true
-   
-   0 // return an integer exit code
-   
-//  let inline findOdd numbers = 
-//  numbers
-//  |> Seq.countBy id
-//  |> Seq.find (fun (_, count) -> count % 2 = 1)
-//  |> fst
-//   
 
-
+ 
