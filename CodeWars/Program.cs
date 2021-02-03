16e6bec93ae31e06f607ab5e37bf1f76489c25dd
fjod <fourjustoneday@yandex.ru>
2021-02-03 12:58:41 +0300

    14. Longest Common Prefix

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -10,21 +10,55 @@ namespace CodeWars
     
     {
         
-        public bool IsPalindrome(int x) {
-            var s = x.ToString().Reverse();
-            StringBuilder sb = new StringBuilder();
-            foreach (var VARIABLE in s)
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            if (!strs.Any()) return "";
+            StringBuilder[] arr = new StringBuilder[strs.Length];
+            for (var i = 0; i < arr.Length; i++)
             {
-                sb.Append(VARIABLE);
+                arr[i] = new StringBuilder();
             }
-            var ret = sb.ToString() == x.ToString();
-            return ret;
+            int counter = 0;
+            int maxLen = strs.Max(l => l.Length);
+            bool prevStringsEqual = false;
+            do
+            {
+                prevStringsEqual = false;
+                var firstString = arr.First().ToString();
+                if (arr.First().Length > 0)
+                {
+                    prevStringsEqual = arr.All(sb => sb.ToString() == firstString);
+                    if (prevStringsEqual == false) return "";
+                }
+                for (int i = 0; i < strs.Length; i++)
+                {
+                    if ( counter<strs[i].Length )
+                        arr[i].Append(strs[i][counter]);
+                }
+                var nextString = arr.First().ToString();
+                
+                    if (arr.Any(sb => sb.ToString() != nextString))
+                    {
+                        if (counter > 0)
+                            return firstString;
+                    }
+                    counter++;
+
+            } while (counter < maxLen+1);
+            if (prevStringsEqual) return arr.First().ToString();
+            return "";
         }
         
         static void Main(string[] args)
         {
-            var q = Reverse(-2147483648);
+            string[] strs = { "a"};
+            Console.WriteLine(LongestCommonPrefix(strs));
             Console.ReadKey();
         }
     }
 }
+
+//I used "vertical scanning" approach, which is pretty good
+//The only problem is that I used it like an idiot
+//There is no need to save strings in StringBuilders and compare contents
+//I can compare chars and in successful case return part of any string from start to current index 
--- CodeWars/Task.md
+++ CodeWars/Task.md
@@ -1,3 +1,15 @@
-﻿An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not.
+﻿Write a function to find the longest common prefix string amongst an array of strings.
 
- 
+If there is no common prefix, return an empty string "".
+
+
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
