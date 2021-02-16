64214f59bf42d166bebc60a2929d345b6da591f6
fjod <fourjustoneday@yandex.ru>
2021-02-16 16:09:57 +0300

    914. X of a Kind in a Deck of Cards

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -10,18 +10,72 @@ namespace CodeWars
     class Program
     {
         
-        public static int SmallestRangeI(int[] A, int K)
+        public static bool HasGroupsSizeX(int[] deck)
         {
-            if (A.Length <= 1) return 0;
-            var sorted = A.ToList().OrderBy(i => i);
-            return Math.Max(sorted.Max() - K*2 - sorted.Min(),0);
+            if (deck.Length <= 1) return false;
+            var q = deck.GroupBy(i => i);
+            if (q.Count() == 1) return true;
+            int? minCount = null;
+            foreach (var group in q)
+            {
+                var currentCount = group.Count();
+                if (minCount.HasValue)
+                {
+                    if (currentCount < minCount)
+                    {
+                        //found new minimalCount
+                        minCount = currentCount;
+                    }
+                    
+                }
+                else
+                    minCount = currentCount;
+            }
+
+            if (minCount == 1) return false;
+
+            int GCD(int a, int b)
+            {
+                return b == 0 ? a : GCD(b, a % b);
+            }
+
+            
+            List<int> gdcs = new List<int>();
+            foreach (var group in q)
+            {
+                var currentCount = group.Count();
+                if (currentCount % minCount != 0)
+                {
+                    int bigCount = Math.Max(currentCount, minCount.Value);
+                    int smallCount = Math.Min(currentCount, minCount.Value);
+                    var gcd = GCD(bigCount, smallCount);
+                    if (gcd > 1)
+                        gdcs.Add(gcd);
+                }
+                else
+                {
+                    gdcs.Add(minCount.Value);
+                }
+                
+            }
+            //code fails for [1,1,1,1,2,2,2,2,2,2]
+            if (gdcs.Count() < 2) return false;
+           
+                var first = gdcs.First();
+                var allTheSame = gdcs.All(i => i == first);
+                return allTheSame;
+            
+       
         }
         static void Main(string[] args)
         {
-            var q = SmallestRangeI(new[] {1, 3,6}, 3);
+            var q = HasGroupsSizeX(new[] {1,1,1,1,2,2,2,2,2,2});
             Console.ReadKey();
         }
     }
 }
 
-//I am too stupid to understand math solution, so here is mine 
+//914. X of a Kind in a Deck of Cards
+//it's too hard :(
+//  return deck.GroupBy(e => e).Select(I => I.Count()).Aggregate(GCD) >= 2;
+//I did not manage to think of to Aggregate func; but was on right track
--- CodeWars/Task.md
+++ CodeWars/Task.md
@@ -1,26 +1,39 @@
-﻿
+﻿In a deck of cards, each card has an integer written on it.
 
+Return true if and only if you can choose X >= 2 such that it is possible to split the entire deck into 1 or more groups of cards, where:
 
-Given an array A of integers, for each integer A[i] we may choose any x with -K <= x <= K, and add x to A[i].
-
-After this process, we have some array B.
-
-Return the smallest possible difference between the maximum value of B and the minimum value of B.
-
+Each group has exactly X cards.
+All the cards in each group have the same integer.
 
 
 Example 1:
 
-Input: A = [1], K = 0
-Output: 0
-Explanation: B = [1]
+Input: deck = [1,2,3,4,4,3,2,1]
+Output: true
+Explanation: Possible partition [1,1],[2,2],[3,3],[4,4].
 Example 2:
 
-Input: A = [0,10], K = 2
-Output: 6
-Explanation: B = [2,8]
+Input: deck = [1,1,1,2,2,2,3,3]
+Output: false´
+Explanation: No possible partition.
 Example 3:
 
-Input: A = [1,3,6], K = 3
-Output: 0
-Explanation: B = [3,3,3] or B = [4,4,4]
+Input: deck = [1]
+Output: false
+Explanation: No possible partition.
+Example 4:
+
+Input: deck = [1,1]
+Output: true
+Explanation: Possible partition [1,1].
+Example 5:
+
+Input: deck = [1,1,2,2,2,2]
+Output: true
+Explanation: Possible partition [1,1],[2,2],[2,2].
+
+
+Constraints:
+
+1 <= deck.length <= 10^4
+0 <= deck[i] < 10^4
