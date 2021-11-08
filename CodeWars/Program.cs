6e6a16921c63b3bb4a087db32c12e064e67cacbf
Michael <fourjustoneday@yandex.ru>
2021-11-08 13:33:22 +0300

    Find Numbers with Even Number of Digits 2

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -5,13 +5,19 @@ namespace CodeWars
 {
     class Program
     {
-        static int maxNumber(int[] nums)
-        {
-            return nums.OrderBy(i => i).Count(i => i is > 9 and < 100 or > 999 and < 10000 or > 99999 and < 1000000);
+        static int FindNumbers(int[] nums) => nums.Count(evenNumOfDigits);
+    
+        static bool evenNumOfDigits(int n) {
+            int count = 0;
+            while(0 < n) {
+                n /= 10;
+                ++count;
+            }
+            return count % 2 == 0;
         }
         static void Main(string[] args)
         {
-            var i = maxNumber(new[] {12,345,2,6,7896});
+            var i = FindNumbers(new[] {12,345,2,6,7896});
             Console.ReadKey();
         }
     }
