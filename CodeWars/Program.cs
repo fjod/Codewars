1840b152f7e09d223b77a1f0362f7b1396e89e78
Michael <fourjustoneday@yandex.ru>
2021-11-26 09:47:38 +0300

    1346. Check If N and Its Double Exist - hashSet

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
@@ -9,33 +11,27 @@ namespace CodeWars
         public static bool CheckIfExist(int[] arr)
         {
             if (arr.Length < 2) return false;
-            
-            bool foundEntry(int search, int shift)
-            {
-                for (int i = shift; i < arr.Length; i++)
-                {
-                    if (arr[i] == search) return true;
-                }
-
-                return false;
-            }
-            for (int i = 0; i < arr.Length; i++)
+            //Loop from i = 0 to arr.length, maintaining in a hashTable the array elements from [0, i - 1].
+            HashSet<int> hs=new HashSet<int>();
+            hs.Add(arr[0]);
+            for (int i = 1; i < arr.Length; i++)
             {
                 if (arr[i] % 2 == 0)
                 {
-                    var currrentDouble2 = arr[i] * 2;
                     var currentDivide2 = arr[i] / 2;
-                    var foundDouble2 = foundEntry(currrentDouble2,i+1);
-                    var foundDiv2 = foundEntry(currentDivide2,i+1);
-                    if (foundDiv2 || foundDouble2) return true;
+                    var currrentDouble2 = arr[i] * 2;
+                    var foundDiv2 = hs.Contains(currentDivide2);
+                    var foundDouble2 =hs.Contains(currrentDouble2);
+                    if (foundDiv2 ) return true;
+                    if (foundDouble2 ) return true;
                 }
                 else
                 {
                     var currrentDouble2 = arr[i] * 2;
-                    var foundDouble2 = foundEntry(currrentDouble2,i+1);
+                    var foundDouble2 =hs.Contains(currrentDouble2);
                     if (foundDouble2) return true;
                 }
-               
+                hs.Add(arr[i]);
             }
 
             return false;
@@ -43,7 +39,7 @@ namespace CodeWars
 
         static void Main(string[] args)
         {
-            var test = new[] {10, 2, 5, 3};
+            var test = new[] {-2,0,10,-19,4,6,-8};
             var ret = CheckIfExist(test); //[0,1,2,3,4,_,_,_,_,_] 5
             test = new[] {3,1,7,11};
             ret = CheckIfExist(test); //[0,1,2,3,4,_,_,_,_,_] 5
