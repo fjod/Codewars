c67663a35e45971fa90d754a99578ac8d96c6aa8
Michael <fourjustoneday@yandex.ru>
2021-12-09 16:02:11 +0300

    283. Move Zeroes

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -8,34 +8,31 @@ namespace CodeWars
 {
     class Program
     {
-        public static int RemoveDuplicates(int[] nums)
-        {
-            if (nums.Length == 1) return 1;
-            if (nums.Length == 0) return 0;
-            if (nums.Length == 2 && nums[0] != nums[1]) return 2;
-            int duplicate = 0;
-            int start = 1;
-            for (int i = 0; i < nums.Length-1; i++)
+        public static void MoveZeroes(int[] nums) {
+        
+            if (nums.Length <= 1) return;
+
+            int retIndex = 0;
+            for (int i = 0; i < nums.Length; i++)
             {
-                var next = nums[i + 1];
-                if (nums[i] == next)
+                if (nums[i] != 0)
                 {
-                    duplicate++;
-                }
-                else
-                {
-                    nums[start] = nums[i + 1];
-                    start++;
+                    nums[retIndex] = nums[i];
+                    retIndex++;
                 }
             }
 
-            return duplicate;
+            for (int i = retIndex; i < nums.Length; i++)
+            {
+                nums[i] = 0;
+            }
+
         }
 
         static void Main(string[] args)
         {
-            var a = new[] {1, 1, 2};
-            var test = RemoveDuplicates(a);
+            var a = new[] {0,1,0,3,12};
+            MoveZeroes(a);
             
             Console.ReadKey();
         }
