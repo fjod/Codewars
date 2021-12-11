c985cb1bd854609141177967926c070883293aa1
Michael <fourjustoneday@yandex.ru>
2021-12-11 10:20:27 +0300

    905. Sort Array By Parity

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -1,38 +1,53 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 
 namespace CodeWars
 {
+   
     class Program
     {
-        public static void MoveZeroes(int[] nums) {
-        
-            if (nums.Length <= 1) return;
+        public static int[] SortArrayByParity(int[] nums)
+        {
 
-            int retIndex = 0;
-            for (int i = 0; i < nums.Length; i++)
+            if (nums.Length <= 1) return nums;
+            int findEvenFromStartIndex(int start)
             {
-                if (nums[i] != 0)
+                for (int i = start; i < nums.Length; i++)
                 {
-                    nums[retIndex] = nums[i];
-                    retIndex++;
+                    if (nums[i] % 2 != 0) return i;
                 }
-            }
 
-            for (int i = retIndex; i < nums.Length; i++)
+                return nums.Length;
+            }
+            
+            int findOddFromEndIndex(int end)
             {
-                nums[i] = 0;
+                for (int i = end; i > 0; i--)
+                {
+                    if (nums[i] % 2 == 0) return i;
+                }
+
+                return 0;
             }
 
+            int start = findEvenFromStartIndex(0);
+            int end = findOddFromEndIndex(nums.Length - 1);
+            while (true)
+            {
+                if (start >= end) return nums;
+                (nums[end], nums[start]) = (nums[start], nums[end]);
+                start = findEvenFromStartIndex(start);
+                end = findOddFromEndIndex(end);
+            }
+
+            
         }
 
+      
         static void Main(string[] args)
         {
-            var a = new[] {0,1,0,3,12};
-            MoveZeroes(a);
+            var a = new[] {3,1,2,4}; // [2,4,3,1]
+            a = SortArrayByParity(a);
             
             Console.ReadKey();
         }
--- CodeWars/Task.md
+++ CodeWars/Task.md
@@ -1,13 +1,14 @@
-﻿Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+﻿Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
 
-Note that you must do this in-place without making a copy of the array.
+Return any array that satisfies this condition.
 
 
 
 Example 1:
 
-Input: nums = [0,1,0,3,12]
-Output: [1,3,12,0,0]
+Input: nums = [3,1,2,4]
+Output: [2,4,3,1]
+Explanation: The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
 Example 2:
 
 Input: nums = [0]
@@ -16,8 +17,5 @@ Output: [0]
 
 Constraints:
 
-1 <= nums.length <= 104
--231 <= nums[i] <= 231 - 1
-
-
-Follow up: Could you minimize the total number of operations done?
+1 <= nums.length <= 5000
+0 <= nums[i] <= 5000
