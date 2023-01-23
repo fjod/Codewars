406fe13cbbbc580253dc02a87349041c36255eec
Michael <fourjustoneday@yandex.ru>
2023-01-23 10:26:26 +0300

    fizzBuzz on F#

--- CodeWars/ConsoleAppFsharp/Program.fs
+++ CodeWars/ConsoleAppFsharp/Program.fs
@@ -2,26 +2,27 @@
 
 module ConsoleAppFsharp =
 
-    let rec SearchBst (node: Option<TreeNode>, target: int) : Option<TreeNode> =
-        match node with
-        | None -> None
-        | Some treeNode ->
-            if (treeNode.V = target) then
-                node
-            else
-                match (treeNode.V, treeNode.Left, treeNode.Right) with
-                | v, Some _, _ when v > target -> SearchBst(treeNode.Left, target)
-                | v, _, Some _ when v < target -> SearchBst(treeNode.Right, target)
-                | _ -> None
+    type FizzBuzz =
+        | Fizz
+        | Buzz
+        | FizzBuzz
+        | Number of int
 
-
-
-    let t1 = TreeNode(1)
-    let t3 = TreeNode(3)
-    let t2 = TreeNode(2, t1, t3)
-    let t7 = TreeNode(7)
-    let t4 = TreeNode(4, t2, t7)
-
-    let test = SearchBst(Some t4, 2)
-
-    printfn "Hello from F#"
+    let checkValue(v : int) =
+        let div3 = (v % 3) = 0
+        let div5 = (v % 5) = 0
+        match (div3, div5) with
+            | true, false -> Fizz
+            | false, true -> Buzz
+            | true, true -> FizzBuzz
+            | _ -> Number v
+            
+    let printFb( f : FizzBuzz) =
+        match f with           
+           | Number v -> v.ToString()
+           | _ -> f.ToString()
+    let generateFb (limit : int) =       
+        {1..limit} |> Seq.map checkValue|> Seq.map printFb |> Seq.map (fun v -> printf $"%A{v},") |> Seq.toList
+        
+    generateFb 100
+    printfn "Hello from F#1"
