5a8f2752c28ed17777b319692c008104ed0e3c58
Michael <fourjustoneday@yandex.ru>
2023-01-24 13:02:04 +0300

    119. Pascal's Triangle II fs dynamic programming with memo

--- CodeWars/ConsoleAppFsharp/Program.fs
+++ CodeWars/ConsoleAppFsharp/Program.fs
@@ -1,28 +1,29 @@
-﻿open TreeNodeDef
+﻿open System
+open System.Collections.Generic
+open TreeNodeDef
 
 module ConsoleAppFsharp =
 
-    type FizzBuzz =
-        | Fizz
-        | Buzz
-        | FizzBuzz
-        | Number of int
-
-    let checkValue(v : int) =
-        let div3 = (v % 3) = 0
-        let div5 = (v % 5) = 0
-        match (div3, div5) with
-            | true, false -> Fizz
-            | false, true -> Buzz
-            | true, true -> FizzBuzz
-            | _ -> Number v
-            
-    let printFb( f : FizzBuzz) =
-        match f with           
-           | Number v -> v.ToString()
-           | _ -> f.ToString()
-    let generateFb (limit : int) =       
-        {1..limit} |> Seq.map checkValue|> Seq.map printFb |> Seq.map (fun v -> printf $"%A{v},") |> Seq.toList
+    let dict = Dictionary<Tuple<int,int>, int>()
+    do
+        dict.Add((0,0), 1)
+        dict.Add((1,0), 1)
+        dict.Add((1,1), 1)
+    let rec calcTriangleValue(v:Tuple<int,int>)=
+        let dictVal = dict.ContainsKey v
+        if dictVal then dict[v]
+        else
+            let left = fst v // row
+            let right = snd v // col
+            match (left, right) with
+                | _, 0 -> 1
+                | _ ,_ when left = right  -> 1
+                | l, r ->
+                    let v = calcTriangleValue((l-1, r-1)) + calcTriangleValue(l-1 ,r)
+                    dict.Add((l,r), v)
+                    v
+    let pascal(row:int) =        
+        [0..row] |> List.map (fun i -> calcTriangleValue(row, i))
         
-    generateFb 100
+    let test = pascal 4
     printfn "Hello from F#1"
--- CodeWars/Task.md
+++ CodeWars/Task.md
@@ -1,5 +1,1 @@
-﻿700. Search in a Binary Search Tree
-
-You are given the root of a binary search tree (BST) and an integer val.
-
-Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
+﻿119. Pascal's Triangle II
