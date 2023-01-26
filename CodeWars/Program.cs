69ce5c20a76ff66aed56fabe2d279517589e2344
Michael <fourjustoneday@yandex.ru>
2023-01-26 13:25:37 +0300

    746. Min Cost Climbing Stairs cs

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -6,47 +6,24 @@ namespace CodeWars
 {
     class Program
     {
-        // top-down (start from goal and recurse down to basecases)
-        public static int Rob(int[] nums)
+        public static int MinCostClimbingStairs(int[] cost)
         {
-            Dictionary<int, int> robVals = new Dictionary<int, int>();
-            robVals[0] = nums[0];
-            robVals[1] = Math.Max(nums[0], nums[1]);
-
-            calcRob(nums.Length - 1, robVals, nums);
-            return robVals[nums.Length - 1];
-        }
-
-        private static int calcRob(int house, Dictionary<int, int> robVals, int[] nums)
-        {
-            if (robVals.ContainsKey(house))
-                return robVals[house];
-            var robPrev = calcRob(house - 1, robVals, nums);
-            var robPrevPrevAndCurrent = calcRob(house - 2, robVals, nums) + nums[house];
-            robVals[house] = Math.Max(robPrev, robPrevPrevAndCurrent);
-            return robVals[house];
-        }
-
-        // bottom-up (start from basecases and go upper towards goal)
-        public static int Rob2(int[] nums)
-        {
-            var robVals = new int[nums.Length];
-            robVals[0] = nums[0];
-            robVals[1] = Math.Max(nums[0], nums[1]);
-            for (int i = 2; i < nums.Length; i++)
+            if (cost.Length == 1) return cost[0];
+            if (cost.Length == 2) return Math.Min(cost[0], cost[1]);
+            int[] costs = new int[cost.Length + 1];
+            costs[0] = cost[0];
+            costs[1] = cost[1];
+            for (int i = 2; i < cost.Length; i++)
             {
-                var robPrev = robVals[i - 1];
-                var robPrevPrevAndCurrent = robVals[i-2] + nums[i];
-                robVals[i] = Math.Max(robPrev, robPrevPrevAndCurrent);
+                costs[i] = cost[i] + Math.Min(costs[i - 1], costs[i - 2]);
             }
+            return Math.Min(costs[cost.Length-1], costs[cost.Length-2]);
 
-            return robVals.Max();
         }
 
         static void Main(string[] args)
         {
-            var test = Rob(new[] {1, 2, 3, 1});
-            test = Rob2(new[] {1, 2, 3, 1});
+            var q = MinCostClimbingStairs(new[] {10, 15, 20});
             Console.ReadKey();
         }
     }
--- CodeWars/Task.md
+++ CodeWars/Task.md
@@ -1,20 +1,29 @@
-﻿198. House Robber
+﻿746. Min Cost Climbing Stairs
 
-You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
 
-Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+You can either start from the step with index 0, or the step with index 1.
+
+Return the minimum cost to reach the top of the floor.
 
 
 
 Example 1:
 
-Input: nums = [1,2,3,1]
-Output: 4
-Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
-Total amount you can rob = 1 + 3 = 4.
-Example 2:
+Input: cost = [10,15,20]
+Output: 15
+Explanation: You will start at index 1.
+- Pay 15 and climb two steps to reach the top.
+  The total cost is 15.
+  Example 2:
 
-Input: nums = [2,7,9,3,1]
-Output: 12
-Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
-Total amount you can rob = 2 + 9 + 1 = 12.
+Input: cost = [1,100,1,1,1,100,1,1,100,1]
+Output: 6
+Explanation: You will start at index 0.
+- Pay 1 and climb two steps to reach index 2.
+- Pay 1 and climb two steps to reach index 4.
+- Pay 1 and climb two steps to reach index 6.
+- Pay 1 and climb one step to reach index 7.
+- Pay 1 and climb two steps to reach index 9.
+- Pay 1 and climb one step to reach the top.
+  The total cost is 6.
