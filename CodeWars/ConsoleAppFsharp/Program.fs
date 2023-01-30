9ebee56957ba4c7251871d318c95b4a31843cdca
Michael <fourjustoneday@yandex.ru>
2023-01-30 18:14:36 +0300

    441. Arranging Coins cs fs

--- CodeWars/ConsoleAppFsharp/Program.fs
+++ CodeWars/ConsoleAppFsharp/Program.fs
@@ -5,26 +5,16 @@ open TreeNodeDef
 
 module ConsoleAppFsharp =
 
-    let rec solve(p1: int, p2 : int, dp: int[][], t1:string, t2 :string) =
-        match (p1, p2) with
-           | p1, _ when p1 = t1.Length -> 0
-           | _, p2 when p2 = t2.Length -> 0
-           | _ ->
-               if dp[p1][p2] <> -1 then dp[p1][p2]
-               else
-                   let ignoreFirst = solve(p1+1, p2, dp, t1, t2)
-                   let mutable takeFirst = 0
-                   let pos = t2.IndexOf(t1[p1], p2)
-                   if pos <> -1 then
-                       takeFirst <- 1 + solve(p1 + 1, p2 + 1, dp, t1, t2)
-                   else takeFirst <- 0
-                   dp[p1][p2] <- Math.Max(ignoreFirst, takeFirst)
-                   dp[p1][p2]
-               
-    let lcs(t1:string, t2:string) =
-        let dp = Array.init t1.Length (fun _ -> Array.init t2.Length (fun _ -> -1))
-        solve(0, 0, dp, t1, t2) |> ignore
-        dp[0][0]
-    
-    let ret = lcs ("ace", "abcde")
-    printfn "Hello from F#1"
+     let rec innerArrange(stair:int, number:int) =
+        match number - stair with
+         | 0 -> stair
+         | v when v < 0 -> stair - 1
+         | v -> innerArrange (v - stair, stair + 1)
+     let arrangeCoins (n:int) =
+        innerArrange (1, n)
+     
+   
+   
+        
+     let test = arrangeCoins 5
+     printfn "Hello from F#1"
--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -8,66 +8,21 @@ namespace CodeWars
 {
     class Program
     {
-        public static int MaximalSquare(char[][] matrix)
+       
+        public static int ArrangeCoins(int n)
         {
-            if (matrix.Length == 0) return 0;
-            if (matrix.Length == 1) return matrix[0].Any(m => m == '1') ? 1 : 0;
-            var maxRow = matrix.Length;
-            var maxCol = matrix[0].Length;
-            var max = Math.Max(maxRow, maxCol);
-            int[][] dp = new int[max][];
-            for (int i = 0; i < max; i++)
+            int stair = 1;
+            while (true)
             {
-                dp[i] = new int[max];
+                n -= stair;
+                if (n == 0) return stair;
+                if (n < 0) return stair - 1;
+                stair += 1;
             }
-
-            var ret = 0;
-            for (int i =0; i < maxRow; i++)
-            {
-                for (int j = 0; j < maxCol; j++)
-                {
-                    if((i==0 || j==0) && matrix[i][j] == '1'){
-                        dp[i][j] = 1;
-                    }
-                    
-                    if (i > 0 && j > 0 && matrix[i - 1][ j - 1] == '1')
-                    {
-                        var up = dp[i - 1][j];
-                        var left = (dp[i][j-1]);
-                        var diag = (dp[i-1][j-1]);
-                        var min =Math.Min(Math.Min(up, left), diag)+1;
-                        dp[i][j] = min;
-                        if (dp[i][j] > ret) ret = dp[i][j];
-                    }
-                }   
-            }
-
-            if (ret == 0)
-                if (matrix.SelectMany(m => m).Any(m => m == '1'))
-                    return 1;
-            return ret * ret;
         }
-
-        static int  toInt(char c)
-        {
-            return c == '1' ? 1 : 0;
-        }
-        
         static void Main(string[] args)
         {
-            char[][] input = new char[5][];
-            input[0] = new char[] {'1', '1', '1', '1', '1'};
-            input[1] = new char[] {'1', '1', '1', '1', '1'};
-            input[2] = new char[] {'0', '0', '0', '0', '0'};
-            input[3] = new char[] {'1', '1', '1', '1', '1'};
-            input[4] = new char[] {'1', '1', '1', '1', '1'};
-            
-            char[][] input2 = new char[2][];
-            input2[0] = new char[] {'1', '1'};
-            input2[1] = new char[] {'1', '1'};
-         
-
-            var ret = MaximalSquare(input);
+            var q = ArrangeCoins(5);
             Console.ReadKey();
         }
     }
