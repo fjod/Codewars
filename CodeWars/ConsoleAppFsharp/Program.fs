4339670b3c75deeaca2e97f3ed053e5ca1cd04a7
Michael <fourjustoneday@yandex.ru>
2023-02-15 14:53:06 +0300

    347.  Top K Frequent Elements

--- CodeWars/ConsoleAppFsharp/Program.fs
+++ CodeWars/ConsoleAppFsharp/Program.fs
@@ -6,19 +6,12 @@ open TreeNodeDef
 module ConsoleAppFsharp =
 
  
-    let twoSum(nums:int[]) (target:int) =
-        let dict = Dictionary<int, int>()
-        nums |> Array.mapi (fun i n ->
-            let left = target - n
-            if dict.ContainsKey(left) then Some (i, dict[left])
-            else                
-                if dict.ContainsKey(n) = false then dict.Add(n, i)
-                None            
-            ) |> Array.choose id |> Array.take 1 |> Array.map (fun r -> [|fst r; snd r|])
+    let topKFreq(nums:int[]) (k:int) =
+        nums
+        |> Array.groupBy id
+        |> Array.map (fun f -> (fst f, (snd f).Length))
+        |> Array.sortByDescending snd
+        |> Array.take k
+        |> Array.map fst
    
-        
-        
-    
-    let test = twoSum [|2;7;11;15|] 9
-    let q = test[0]
     printfn "Hello from F#1"
--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -10,56 +10,21 @@ namespace CodeWars
 {
     class Program
     {
-        public static IList<IList<string>> GroupAnagrams(string[] strs)
+        public static int[] TopKFrequent(int[] nums, int k)
         {
-            List<(string, int[])> dict = new List<(string, int[])>();
-            for (int i = 0; i < strs.Length; i++)
-            {
-                dict.Add((strs[i], CreateArray(strs[i])));
-            }
-
-            var test = dict.GroupBy(g => CreateHashCode(g.Item2)).ToList();
-            IList<IList<string>> ret = new List<IList<string>>();
-            foreach (var v in test)
-            {
-                ret.Add(new List<string>(v.Select(z => z.Item1)));
-            }
-
-            return ret;
-        }
-
-        private static StringBuilder sb = new StringBuilder();
-        private static int CreateHashCode(int[] i)
-        {
-            sb.Clear();
-            for (int j = 0; j < i.Length; j++)
-            {
-                if (i[j] != 0)
-                {
-                    sb.Append(j);
-                    sb.Append(i[j]*17);
-                }
-            }
-
-            return sb.ToString().GetHashCode();
-        }
-        private static int[] CreateArray(string s)
-        {
-            int[] ret = new int[26];
-            for (var i = 0; i < s.Length; i++)
-            {
-                var c = s[i];
-                var index = c - 'a';
-                ret[index]++;
-            }
-
-            return ret;
+            return nums
+                .GroupBy(g => g)
+                .Select(g => (g.Key, g.Count()))
+                .OrderByDescending(g => g.Item2)
+                .Take(k)
+                .Select(z => z.Key)
+                .ToArray();
         }
 
 
         static void Main(string[] args)
         {
-            var test = GroupAnagrams(new[] {"abbbbbbbbbbb","aaaaaaaaaaab"});
+            var test = TopKFrequent(new[] {1,1,1,2,2,3}, 2);
             Console.ReadKey();
         }
     }
