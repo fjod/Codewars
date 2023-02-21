130fe57b307b79c7b1d30cc2ac9515c18bf8700b
Michael <fourjustoneday@yandex.ru>
2023-02-21 12:25:16 +0300

    739. Daily Temperatures cs

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -10,44 +10,49 @@ namespace CodeWars
 {
     class Program
     {
-        public static int EvalRPN(string[] tokens)
+
+        public static int[] DailyTemperatures(int[] temperatures)
         {
-            if (tokens.Length == 1) return int.Parse(tokens.First());
-            Stack<int> vals = new Stack<int>();
-            int result = 0;
-            for (int c = 0; c < tokens.Length; c++)
+            Stack<(int temp, int index)> stack = new Stack<(int temp, int index)>(temperatures.Length);
+            int[] ret = new int[temperatures.Length];
+            int counter = 0;
+            foreach (var temperature in temperatures)
             {
-                if (tokens[c] == "+" || tokens[c] == "-" || tokens[c] == "/" || tokens[c] == "*")
+                if (stack.Count == 0)
                 {
-                    var v1 = vals.Pop();
-                    var v2 = vals.Pop();
-                    result = calc(tokens[c], v1, v2);
-                    vals.Push(result);
+                    stack.Push((temperature, 0));
+                    counter++;
                 }
                 else
                 {
-                    vals.Push(int.Parse(tokens[c]));
+                    var prev = stack.Peek();
+                    do
+                    {
+                        prev = stack.Peek();
+                        if (prev.temp < temperature)
+                        {
+                            stack.Pop();
+                            var targetLength = counter - prev.index;
+                            ret[prev.index] = targetLength;
+                        }
+                        else break;
+                        if (stack.Count == 0) break;
+                        
+                    } while (prev.temp < temperature);
+                   
+                    stack.Push((temperature, counter));
+                    counter++;
                 }
             }
 
-            return result;
-        }
-
-        private static int calc(string i, int v1, int v2)
-        {
-            return i switch
-            {
-                "+" => v1 + v2,
-                "-" => v2 - v1,
-                "*" => v1 * v2,
-                "/" => v2 / v1
-            };
+            return ret;
         }
 
 
         static void Main(string[] args)
         {
-            var q = EvalRPN(new[] {"2", "1", "+", "3", "*"});
+                                                // [1,1,4,2,1,1,0,0]
+            var q = DailyTemperatures(new []{73,74,75,71,69,72,76,73});
             Console.ReadKey();
         }
     }
