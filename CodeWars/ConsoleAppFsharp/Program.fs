c6aa089cd9bc099dae60bdc29cab2ad8e178b2e6
Michael <fourjustoneday@yandex.ru>
2023-03-15 14:18:57 +0300

    39. Combination Sum cs fs

--- CodeWars/ConsoleAppFsharp/Program.fs
+++ CodeWars/ConsoleAppFsharp/Program.fs
@@ -16,5 +16,23 @@ module ConsoleAppFsharp =
                  backTrack subset ret2 l1
          backTrack [] [] 0
      
+     let permute (num:int[])=
+         let rec backTrack (num:int[]) (ret : int list list) (level:int)=
+             if num.Length = level then
+                    let lst = num |> Array.toList 
+                    lst :: ret
+                else
+                    let lst = [level..num.Length]
+                    for i in lst do
+                          let numsI = num[i]
+                          let numLevel = num[level]
+                          num[level] <- numsI
+                          num[i] <- numLevel                       
+                          backTrack num ret (level + 1) |> ignore
+                          num[level] <- numLevel
+                          num[i] <- numsI           
+                    ret
+         backTrack num [] 0 
+     
      let test = subset [|1;2;3|]
      printfn "Hello from F#1"
--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -11,35 +11,32 @@ namespace CodeWars
 {
     class Program
     {
-        static readonly IList<IList<int>> result = new List<IList<int>>();
-
-        private static void backTrack(int index, int total, int[] candidates, int target, List<int> path)
-        {
-            if (total == target)
-            {
-                result.Add(path.ToList());
-                return;
-            }
-
-            if (total > target || index >= candidates.Length) return;
-            path.Add(candidates[index]);
-            backTrack(index, total + candidates[index], candidates, target, path);
-            path.Remove(path.Last());
-            backTrack(index+1, total, candidates, target, path);
-        }
-        public static IList<IList<int>> CombinationSum(int[] candidates, int target)
-        {
-
-            backTrack(0, 0, candidates, target, new List<int>());
-            return result;
-        }
             
+        public static IList<IList<int>> Permute(int[] nums)
+        {
+            List<IList<int>> ret = new List<IList<int>>();
+            InnerPermute(ret, nums, 0);
+            return ret;
+        }
+
+
+        static void InnerPermute(IList<IList<int>> ret, int[] nums, int level)
+        {
+            if (level == nums.Length)ret.Add(new List<int>(nums));
+            else
+            {
+                for (int i = level; i < nums.Length; i++)
+                {
+                    (nums[i], nums[level]) = (nums[level], nums[i]);
+                    InnerPermute(ret, nums, level + 1);
+                    (nums[i], nums[level]) = (nums[level], nums[i]);
+                }
+            }
+        }
         
-
-
         static void Main(string[] args)
         {
-            var q = CombinationSum(new[] {2,3,6,7}, 7);
+            var q = Permute(new[] {1,2,3});
             Console.ReadKey();
         }
     }
