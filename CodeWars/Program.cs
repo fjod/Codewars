261d2914038c3fc87c5e2bb49fc6fe30c62e495a
Michael <fourjustoneday@yandex.ru>
2023-03-20 12:19:54 +0300

    134. Gas Station

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -11,59 +11,28 @@ namespace CodeWars
 {
     class Program
     {
-        public static bool CanJump(int[] nums)
+        public static int CanCompleteCircuit(int[] gas, int[] cost)
         {
-            if (nums.Length == 1)
-                return (nums.First() == 0 || nums.First() == 1);
-            return JumpInner3(nums);
-        }
-        
-        public static bool CanJump2(int[] nums) {
-            int goal = nums.Length - 1;
-            for (int i = nums.Length-1; i>=0; i--)
+
+            if (gas.Sum() < cost.Sum()) return -1;
+            int ret = 0;
+            int total = 0;
+            for (int i = 0; i < gas.Length; i++)
             {
-                if (nums[i] + i >= goal) //можно ли с этой позиции прыгнуть до или дальше цели?
+                total += gas[i] - cost[i];
+                if (total < 0) // если текущая сумма < 0, то начинать надо не отсюда (дальше не уедем)
                 {
-                    goal = i; //если можно, то смещаем цель ближе к началу массива
+                    ret = i + 1;
+                    total = 0;
                 }
             }
 
-            return goal == 0  ;
-        }
-
-        //Time Limit Exceeded
-        private static Dictionary<int, bool> mem = new Dictionary<int, bool>(); 
-        private static bool JumpInner(int[] nums)
-        {
-            if (nums.Length == 0) return false;
-            if (mem.ContainsKey(nums.Length)) return false;
-            for (int i = nums[0]; i > 0; i--)
-            {
-                if (i >= nums.Length - 1) return true;
-                var skippedArr = nums.Skip(i).ToArray();
-                if (JumpInner(skippedArr)) return true;
-            }
-            mem.Add(nums.Length, false);
-            return false;
-        }
-
-        // не работает с [2,0,0]
-        private static bool JumpInner3(int[] nums)
-        {
-            if (nums.Length == 2)
-            {
-                if (nums.First() >= 1) return true;
-                return false;
-            }
-
-            var secondBeforeLast = nums[^2];
-            if (secondBeforeLast >= 1) return JumpInner3(nums.Take(nums.Length - 1).ToArray());
-            return false;
+            return ret;
         }
         
         static void Main(string[] args)
         {
-            var q = CanJump(new[] {2, 3, 1, 1, 4});
+            var q = CanCompleteCircuit(new[] {2, 3, 1, 1, 4});
 
             Console.ReadKey();
         }
--- CodeWars/Task.md
+++ CodeWars/Task.md
@@ -1,10 +1,12 @@
-﻿55. Jump Game
-    Medium
-    15.6K
-    800
-    Companies
-    You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+﻿134. Gas Station
+     Medium
+     9.6K
+     813
+     Companies
+     There are n gas stations along a circular route, where the amount of gas at the ith station is gas[i].
 
-Return true if you can reach the last index, or false otherwise.
+You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from the ith station to its next (i + 1)th station. You begin the journey with an empty tank at one of the gas stations.
+
+Given two integer arrays gas and cost, return the starting gas station's index if you can travel around the circuit once in the clockwise direction, otherwise return -1. If there exists a solution, it is guaranteed to be unique
 
  
