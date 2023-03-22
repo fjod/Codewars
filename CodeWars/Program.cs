1f7940412fab3e88ce162f78336ae2a9452a9628
Michael <fourjustoneday@yandex.ru>
2023-03-22 12:52:08 +0300

    417. Pacific Atlantic Water Flow (does not pass all cases)

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -12,47 +12,86 @@ namespace CodeWars
 {
     class Program
     {
-        private Dictionary<(int, int), bool> visited = new Dictionary<(int, int), bool>();
-        private int current;
-        private int max;
-        private int maxLen;
-        private int maxWidth;
-        public int MaxAreaOfIsland(int[][] grid)
+        //67 / 113 testcases passed for my solution
+        private static int maxLen;
+        private static int maxWidth;
+        private static List<IList<int>> ret = new List<IList<int>>();
+        public static IList<IList<int>> PacificAtlantic(int[][] heights)
         {
-            maxLen = grid.Length;
-            maxWidth = grid.First().Length;
-            for (int i = 0; i < grid.Length; i++)
+            maxLen = heights.Length;
+            maxWidth = heights.First().Length;
+            for (int i = 0; i < heights.Length; i++)
             {
-                for (int j = 0; j < grid[i].Length; j++)
+                for (int j = 0; j < heights[i].Length; j++)
                 {
-                    if (visited.ContainsKey((i,j))) continue;
-                    dfs(i, j, grid);
-                    max = Math.Max(current, max);
-                    current = 0;
+                    visitedPacific.Clear();
+                    visitedAtlantic.Clear();
+                    var pacific = CanGoPacific(i, j, heights, int.MaxValue);
+                    var atlantic = CanGoAtlantic(i, j, heights, int.MaxValue);
+                    var newList = new List<int>(2) {i, j};
+                    if (pacific && atlantic && !ret.Any(l => l.SequenceEqual(newList)))
+                    {
+                        ret.Add(newList);
+                    }
                 }
             }
 
-            return max;
+            return ret;
         }
 
-        private void dfs(int i, int j, int[][] grid)
+        private static Dictionary<(int, int), bool> visitedPacific = new Dictionary<(int, int), bool>(); 
+        static bool CanGoPacific(int i, int j, int[][] grid, int prevCellValue)
         {
-            if (i < 0 || i >= maxLen) return;
-            if (j < 0 || j >= maxWidth) return;
-            if (grid[i][j] == 1 && !visited.ContainsKey((i,j)))
-            {
-                visited.Add((i,j), true);
-                current += 1;
-                dfs(i +1 , j, grid);
-                dfs(i -1 , j, grid);
-                dfs(i  , j +1, grid);
-                dfs(i  , j - 1, grid);
-            }
+            if (visitedPacific.ContainsKey((i, j))) return false;
+            visitedPacific.Add((i,j), true);
+            if (i < 0 || i >= maxLen) return false;
+            if (j < 0 || j >= maxWidth) return false;
+            var current = grid[i][j];
+            if (i == 0 && current <= prevCellValue) return true;
+            if (j == 0 && current <= prevCellValue) return true;
+            
+            if (current > prevCellValue) return false;
+            var goUp = CanGoPacific(i - 1, j, grid, current);
+            var goLeft =   CanGoPacific(i , j - 1, grid, current);
+            var goRight =   CanGoPacific(i , j + 1, grid, current);
+            return goUp || goLeft || goRight;
         }
 
+        private static Dictionary<(int, int), bool> visitedAtlantic = new Dictionary<(int, int), bool>();
+        static bool CanGoAtlantic(int i, int j, int[][] grid, int prevCellValue)
+        {
+            if (visitedAtlantic.ContainsKey((i, j))) return false;
+            visitedAtlantic.Add((i,j), true);
+            if (i < 0 || i >= maxLen) return false;
+            if (j < 0 || j >= maxWidth) return false;
+            var current = grid[i][j];
+            if (i == maxLen - 1 && current <= prevCellValue) return true;
+            if (j == maxWidth - 1 && current <= prevCellValue) return true;
+          
+            if (current > prevCellValue) return false;
+            var goUp = CanGoAtlantic(i+ 1, j, grid, current);
+            var goLeft = CanGoAtlantic(i , j + 1, grid, current);
+            var goRight = CanGoAtlantic(i , j - 1, grid, current);
+            return goUp || goLeft || goRight;
+        }
+        
         static void Main(string[] args)
         {
-
+            var q = PacificAtlantic(new[]
+            {
+                new []{1,2,2,3,5},
+                new []{3,2,3,4,4},
+                new []{2,4,5,3,1},
+                new []{6,7,1,4,5},
+                new []{5,1,1,2,4}
+            });
+            
+            var q1 = PacificAtlantic(new[]
+            {
+                new []{1,2,3},
+                new []{8,9,4},
+                new []{7,6,5},
+            });
             Console.ReadKey();
         }
     }
--- CodeWars/Task.md
+++ CodeWars/Task.md
@@ -1,10 +1,14 @@
-﻿695. Max Area of Island
+﻿417. Pacific Atlantic Water Flow
      Medium
-     8.7K
-     195
+     6.2K
+     1.2K
      Companies
-     You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+     There is an m x n rectangular island that borders both the Pacific Ocean and Atlantic Ocean. The Pacific Ocean touches the island's left and top edges, and the Atlantic Ocean touches the island's right and bottom edges.
 
-The area of an island is the number of cells with a value 1 in the island.
+The island is partitioned into a grid of square cells. You are given an m x n integer matrix heights where heights[r][c] represents the height above sea level of the cell at coordinate (r, c).
 
-Return the maximum area of an island in grid. If there is no island, return 0.
+The island receives a lot of rain, and the rain water can flow to neighboring cells directly north, south, east, and west if the neighboring cell's height is less than or equal to the current cell's height. Water can flow from any cell adjacent to an ocean into the ocean.
+
+Return a 2D list of grid coordinates result where result[i] = [ri, ci] denotes that rain water can flow from cell (ri, ci) to both the Pacific and Atlantic oceans.
+
+ 
