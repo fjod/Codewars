1a19ad006b43b0610cae0f14043879fa6cccf502
Michael <fourjustoneday@yandex.ru>
2023-03-28 13:19:41 +0300

    5. Longest Palindromic Substring

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -13,71 +13,40 @@ namespace CodeWars
 {
     class Program
     {
-        public static bool CanFinish(int numCourses, int[][] prerequisites)
+        public static string LongestPalindrome(string s)
         {
-            Dictionary<int, List<int>> coursesPrereqs = new Dictionary<int, List<int>>(numCourses - 1);
-            HashSet<int> visited = new HashSet<int>(numCourses - 1);
-            for (int i = 0; i < numCourses; i++)
+            int high = 0;
+            int low = 0;
+            
+            for (int i = 0; i < s.Length; i++)
             {
-                var preqs = prerequisites.Where(p => p[0] == i).Select(p => p[1]).ToList();
-                coursesPrereqs.Add(i, preqs);
-            }
-
-            foreach (int c in Enumerable.Range(0, numCourses))
-            {
-                visited.Clear();
-                if (!checkCourse(c, coursesPrereqs, visited))
+                int current = Math.Max(TestPalindrome(s, i, i), TestPalindrome(s, i, i + 1));
+                if (current > high - low)
                 {
-                    return false;
+                    high = i + current / 2;
+                    low = i - (current-1) / 2;
                 }
             }
-            return true;
+
+            return s.Substring(low, high -low  + 1);
         }
 
-        private static Dictionary<int, bool> canFinish = new Dictionary<int, bool>();
-        private static bool checkCourse(int i, Dictionary<int, List<int>> coursesPrereqs, HashSet<int> visited)
+        static int  TestPalindrome(string s,  int low, int high)
         {
-            if (visited.Contains(i)) return false;
-            var prequesForTheCourse = coursesPrereqs[i];
-            if (!prequesForTheCourse.Any()) return true;
-            visited.Add(i);
-            foreach (var i1 in prequesForTheCourse)
+            while (low >=0 && high< s.Length && s[high] == s[low])
             {
-                if (!canFinish.ContainsKey(i1))
-                {
-                    var course = checkCourse(i1, coursesPrereqs, visited);
-                    if (!course)
-                    {
-                        return false;
-                    }
-
-                    canFinish.Add(i1, true);
-                    visited.Remove(i1);
-                }
-                else return canFinish[i1];
+                high++;
+                low--;
             }
 
-            return true;
+            return high - low - 1;
         }
+     
 
 
         static void Main(string[] args)
         {
-            var q2 = CanFinish(8, new[]
-            {
-                new []{1,0},
-                new []{0,1},
-            });
-            var q1 = CanFinish(8, new[]
-            {
-                new []{1,0},
-                new []{2,6},
-                new []{1,7},
-                new []{6,4},
-                new []{7,0},
-                new []{0,5},
-            });
-
+            var test = LongestPalindrome("babad");
             Console.ReadKey();
         }
     }
--- CodeWars/Task.md
+++ CodeWars/Task.md
@@ -1,9 +1,23 @@
-﻿207. Course Schedule
-     Medium
-     12.9K
-     512
-     Companies
-     There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+﻿5. Longest Palindromic Substring
+   Medium
+   24.5K
+   1.4K
+   Companies
+   Given a string s, return the longest
+   palindromic
 
-For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
-Return true if you can finish all courses. Otherwise, return false.
+substring
+in s.
+
+
+
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+Example 2:
+
+Input: s = "cbbd"
+Output: "bb"
+ 
