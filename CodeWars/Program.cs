b9a3720516c7bef85d510be6ac5d1af246cad51b
Michael <fourjustoneday@yandex.ru>
2023-04-17 10:35:02 +0300

    8. String to Integer (atoi) - this problem is really weird

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -13,90 +13,50 @@ namespace CodeWars
 {
     class Program
     {
-        //1057 / 1157 testcases passed
-        public static string Convert(string s, int numRows)
+        public int MyAtoi(string s)
         {
-            if (numRows == 1) return s;
-
-            List < List<int>> arr = new List<List<int>>();
-            for (int i = 0; i < numRows; i++)
+            int sign = 1;
+            var trimmed = s.Trim();
+            if (trimmed.StartsWith('-'))
             {
-                arr.Add(Enumerable.Repeat(int.MaxValue, 100).ToList());
+                sign = -1;
+                s = s.Substring(1);
             }
 
-            int current = 0;
-            bool isDown = true;
-            int columnShift = 0;
-            int rowShift = 1;
+            if (trimmed.StartsWith('+'))
+            {
+                s = s.Substring(1);
+            }
+
+            string conv = String.Empty;
             for (int i = 0; i < s.Length; i++)
             {
-                if (current < numRows && isDown == true)
+                var c = s[i];
+                if (c >= '0' && c <= '9')
                 {
-                    arr[current][columnShift] = i;
-                    current++;
-                }
-                else if (current >= 1 && isDown == false)
-                {
-                    current--;
-                    arr[current][columnShift+rowShift] = i;
-                    rowShift++;
-                }
-
-
-                if (current == numRows && isDown)
-                {
-                    isDown = false;
-                    current--;
-                };
-                if (current == 1 && !isDown)
-                {
-                    isDown = true;
-                    columnShift+=numRows-1;
-                    rowShift = 1;
-                    current = 0;
+                    conv += c;
                 }
             }
 
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < arr.Count; i++)
+            if (conv.Length == 0)
             {
-                for (int j = 0; j < arr[i].Count; j++)
-                {
-                    var current2 = arr[i][j];
-                    if (current2 != int.MaxValue)
-                    {
-                        sb.Append(s[current2]);
-                    }
-                }
+                return 0;
             }
 
-            return sb.ToString();
+            conv = conv.TrimStart('0');
+            var parsed = long.Parse(conv) * sign;
+            if (parsed > int.MaxValue)
+            {
+                return int.MaxValue;
+            }
+            if (parsed < int.MinValue)
+            {
+                return int.MinValue;
+            }
+
+            return (int)parsed;
         }
-        
-        public string Convert2(string s, int numRows) {
-            if(numRows == 1 || numRows >= s.Length)
-            {
-                return s;
-            }
-            string[] res = new string[numRows];
-            int index = 0;
-            int step = 1;
-            for(int i = 0; i<s.Length;i++)
-            {
-                res[index] += s[i];
-                if(index == 0)
-                {
-                    step = 1;
-                }
-                else if(index == numRows - 1)
-                {
-                    step = -1;
-                }
-                index += step;
-            }
-            return string.Join(string.Empty, res);
-        }
-    
+
 
 
         static void Main(string[] args)
