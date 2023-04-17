cc2db0f3015dcb175764f1f220bdf7c56d41d5d5
Michael <fourjustoneday@yandex.ru>
2023-04-17 09:47:09 +0300

    6. Zigzag Conversion

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -13,35 +13,95 @@ namespace CodeWars
 {
     class Program
     {
-        
-        public int[] CountBits(int n)
+        //1057 / 1157 testcases passed
+        public static string Convert(string s, int numRows)
         {
+            if (numRows == 1) return s;
 
-            int[] ret = new int[n+1];
-            for (int i = 0; i < n+1; i++)
+            List < List<int>> arr = new List<List<int>>();
+            for (int i = 0; i < numRows; i++)
             {
-                var asBin = Convert.ToString(i, 2);
-                ret[i] = asBin.Count(s => s == '1');
-                ret[i] = solve(i);
+                arr.Add(Enumerable.Repeat(int.MaxValue, 100).ToList());
             }
 
-            return ret;
+            int current = 0;
+            bool isDown = true;
+            int columnShift = 0;
+            int rowShift = 1;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (current < numRows && isDown == true)
+                {
+                    arr[current][columnShift] = i;
+                    current++;
+                }
+                else if (current >= 1 && isDown == false)
+                {
+                    current--;
+                    arr[current][columnShift+rowShift] = i;
+                    rowShift++;
+                }
+
+
+                if (current == numRows && isDown)
+                {
+                    isDown = false;
+                    current--;
+                };
+                if (current == 1 && !isDown)
+                {
+                    isDown = true;
+                    columnShift+=numRows-1;
+                    rowShift = 1;
+                    current = 0;
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < arr.Count; i++)
+            {
+                for (int j = 0; j < arr[i].Count; j++)
+                {
+                    var current2 = arr[i][j];
+                    if (current2 != int.MaxValue)
+                    {
+                        sb.Append(s[current2]);
+                    }
+                }
+            }
+
+            return sb.ToString();
         }
         
-        public int solve(int n){
-            // base condition
-            if(n == 0) return 0;
-            if(n == 1) return 1;
-        
-            if(n % 2 == 0) return solve(n / 2); // handling even case
-            else return 1 + solve(n / 2); // handling odd case
+        public string Convert2(string s, int numRows) {
+            if(numRows == 1 || numRows >= s.Length)
+            {
+                return s;
+            }
+            string[] res = new string[numRows];
+            int index = 0;
+            int step = 1;
+            for(int i = 0; i<s.Length;i++)
+            {
+                res[index] += s[i];
+                if(index == 0)
+                {
+                    step = 1;
+                }
+                else if(index == numRows - 1)
+                {
+                    step = -1;
+                }
+                index += step;
+            }
+            return string.Join(string.Empty, res);
         }
-    }
+    
 
 
         static void Main(string[] args)
         {
-        
+            var q = Convert("PAYPALISHIRING", 3);
         }
     }
 }
--- CodeWars/Task.md
+++ CodeWars/Task.md
@@ -1,33 +1,41 @@
-﻿338. Counting Bits
-     Easy
-     8.7K
-     412
-     Companies
-     Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+﻿6. Zigzag Conversion
+   Medium
+   6K
+   12.1K
+   Companies
+   The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
 
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string s, int numRows);
 
 
 Example 1:
 
-Input: n = 2
-Output: [0,1,1]
-Explanation:
-0 --> 0
-1 --> 1
-2 --> 10
+Input: s = "PAYPALISHIRING", numRows = 3
+Output: "PAHNAPLSIIGYIR"
 Example 2:
 
-Input: n = 5
-Output: [0,1,1,2,1,2]
+Input: s = "PAYPALISHIRING", numRows = 4
+Output: "PINALSIGYAHRPI"
 Explanation:
-0 --> 0
-1 --> 1
-2 --> 10
-3 --> 11
-4 --> 100
-5 --> 101
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+Example 3:
+
+Input: s = "A", numRows = 1
+Output: "A"
 
 
 Constraints:
 
-0 <= n <= 105
+1 <= s.length <= 1000
+s consists of English letters (lower-case and upper-case), ',' and '.'.
+1 <= numRows <= 1000
