8ecc8e134c71142c1e3ca8c89a6dc2aa18f8b331
Michael <fourjustoneday@yandex.ru>
2023-04-18 14:27:53 +0300

    55. Jump Game

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -13,59 +13,74 @@ namespace CodeWars
 {
     class Program
     {
-        public static int[] SearchRange(int[] nums, int target)
+        // I mistook greedy for dp
+        public static bool CanJump(int[] nums)
         {
+            int maxRight = 0;
 
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i > maxRight) { return false; } // we cant get to this pos
+                if (i + nums[i] > maxRight) // can we jump more then previously?
+                    maxRight = i + nums[i]; // if we can, save this position
+                if (maxRight >= nums.Length - 1) // if we arrived to the end then return
+                    return true;
+            }
+
+            return false;
+        }
+        
+        public static bool CanJump2(int[] nums) {
             if (nums.Length == 1)
+                return (nums.First() == 0 || nums.First() == 1);
+            return JumpInner2(nums);
+        }
+  
+        // prev code 136 / 171 testcases passed 
+        private  bool JumpInner(int[] nums)
+        {
+            if (nums.Length == 2)
             {
-                if (nums[0] == target)
-                {
-                    return new[] {0, 0};
-                }
-                return new[] {-1, -1};
+                if (nums.First() >= 1) return true;
+                return false;
             }
-            List<int> ret = new List<int>();
-            int left = 0;
-            int right = nums.Length - 1;
-            
-            while (left <= right)
-            {
-                int mid = (left + right)/2;
-                if (nums[mid] == target)
-                { //search left and right for termination
-                    int l = mid;
-                    int r = mid;
-                    while (l >= 0 && nums[l] == target)
-                    {
-                        l--;
-                    }
-                    while (r < nums.Length && nums[r] == target)
-                    {
-                        r++;
-                    }
 
-                    ret.Add(l+1);
-                    ret.Add(r-1);
-                    return ret.ToArray();
-                }
-                else if (nums[mid] < target)
-                {
-                    left = mid+ 1;
-                }
-                else
-                {
-                    right = mid - 1;
-                }
+            var secondBeforeLast = nums[^2];
+            if (secondBeforeLast >= 1) return JumpInner(nums.Take(nums.Length - 1).ToArray());
+            return false;
+        }
+
+        // 73 / 171 testcases passed
+        private static HashSet<int> memo = new HashSet<int>();
+        private static bool JumpInner2(int[] nums)
+        {
+            if (memo.Contains(nums.GetHashCode())) return false;
+            if (nums.Length == 2)
+            {
+                if (nums.First() >= 1) return true;
+                memo.Add(nums.GetHashCode());
+                return false;
             }
-            
-            ret.Add(-1);
-            ret.Add(-1);
-            return ret.ToArray();
+            if (nums.Length == 1) return true;
+            var first = nums[0];
+            if (first == 0)
+            {
+                memo.Add(nums.GetHashCode());
+                return false;
+
+            };
+            for (int i = 1; i <= first; i++)
+            {
+                if (JumpInner2(nums.Skip(i).ToArray())) return true;
+            }
+
+            memo.Add(nums.GetHashCode());
+            return false;
         }
 
         static void Main(string[] args)
         {
-            var q = SearchRange(new []{2,2}, 2);
+            var q = CanJump(new []{3,2,1,0,4});
         }
     }
 }
--- CodeWars/Task.md
+++ CodeWars/Task.md
@@ -1,33 +1,27 @@
-﻿34. Find First and Last Position of Element in Sorted Array
+﻿55. Jump Game
     Medium
-    16.6K
-    394
+    15.9K
+    813
     Companies
-    Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+    You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
 
-If target is not found in the array, return [-1, -1].
-
-You must write an algorithm with O(log n) runtime complexity.
+Return true if you can reach the last index, or false otherwise.
 
 
 
 Example 1:
 
-Input: nums = [5,7,7,8,8,10], target = 8
-Output: [3,4]
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
 Example 2:
 
-Input: nums = [5,7,7,8,8,10], target = 6
-Output: [-1,-1]
-Example 3:
-
-Input: nums = [], target = 0
-Output: [-1,-1]
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
 
 
 Constraints:
 
-0 <= nums.length <= 105
--109 <= nums[i] <= 109
-nums is a non-decreasing array.
--109 <= target <= 109
+1 <= nums.length <= 104
+0 <= nums[i] <= 105
