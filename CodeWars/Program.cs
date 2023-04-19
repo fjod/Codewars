70af776569154fce3c1e4fcca2d2c0a6a5c051ba
Michael <fourjustoneday@yandex.ru>
2023-04-19 13:40:26 +0300

    48. Rotate Image

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -13,74 +13,31 @@ namespace CodeWars
 {
     class Program
     {
-        // I mistook greedy for dp
-        public static bool CanJump(int[] nums)
-        {
-            int maxRight = 0;
-
-            for (int i = 0; i < nums.Length; i++)
-            {
-                if (i > maxRight) { return false; } // we cant get to this pos
-                if (i + nums[i] > maxRight) // can we jump more then previously?
-                    maxRight = i + nums[i]; // if we can, save this position
-                if (maxRight >= nums.Length - 1) // if we arrived to the end then return
-                    return true;
-            }
-
-            return false;
-        }
+        public static void Rotate(int[][] matrix) {
         
-        public static bool CanJump2(int[] nums) {
-            if (nums.Length == 1)
-                return (nums.First() == 0 || nums.First() == 1);
-            return JumpInner2(nums);
-        }
-  
-        // prev code 136 / 171 testcases passed 
-        private  bool JumpInner(int[] nums)
-        {
-            if (nums.Length == 2)
+            //write function to transpose matrix
+            for (int i = 0; i < matrix.Length; i++)
             {
-                if (nums.First() >= 1) return true;
-                return false;
+                for (int j = i; j < matrix[i].Length; j++)
+                {
+                    (matrix[i][j], matrix[j][i]) = (matrix[j][i], matrix[i][j]);
+                }
             }
-
-            var secondBeforeLast = nums[^2];
-            if (secondBeforeLast >= 1) return JumpInner(nums.Take(nums.Length - 1).ToArray());
-            return false;
-        }
-
-        // 73 / 171 testcases passed
-        private static HashSet<int> memo = new HashSet<int>();
-        private static bool JumpInner2(int[] nums)
-        {
-            if (memo.Contains(nums.GetHashCode())) return false;
-            if (nums.Length == 2)
+            
+            //write function to reverse matrix
+            for (int i = 0; i < matrix.Length; i++)
             {
-                if (nums.First() >= 1) return true;
-                memo.Add(nums.GetHashCode());
-                return false;
+                for (int j = 0; j < matrix[i].Length / 2; j++)
+                {
+                    (matrix[i][j], matrix[i][matrix[i].Length - 1 - j]) = (matrix[i][matrix[i].Length - 1 - j], matrix[i][j]);
+                }
+
             }
-            if (nums.Length == 1) return true;
-            var first = nums[0];
-            if (first == 0)
-            {
-                memo.Add(nums.GetHashCode());
-                return false;
-
-            };
-            for (int i = 1; i <= first; i++)
-            {
-                if (JumpInner2(nums.Skip(i).ToArray())) return true;
-            }
-
-            memo.Add(nums.GetHashCode());
-            return false;
         }
 
         static void Main(string[] args)
         {
-            var q = CanJump(new []{3,2,1,0,4});
+            Rotate(new []{new []{1,2,3}, new []{4,5,6}, new []{7,8,9}});
         }
     }
 }
--- CodeWars/Task.md
+++ CodeWars/Task.md
@@ -1,27 +1,28 @@
-﻿55. Jump Game
+﻿48. Rotate Image
     Medium
-    15.9K
-    813
+    14.3K
+    641
     Companies
-    You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+    You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
 
-Return true if you can reach the last index, or false otherwise.
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
 
 
 
 Example 1:
 
-Input: nums = [2,3,1,1,4]
-Output: true
-Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[7,4,1],[8,5,2],[9,6,3]]
 Example 2:
 
-Input: nums = [3,2,1,0,4]
-Output: false
-Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+
+Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
 
 
 Constraints:
 
-1 <= nums.length <= 104
-0 <= nums[i] <= 105
+n == matrix.length == matrix[i].length
+1 <= n <= 20
+-1000 <= matrix[i][j] <= 1000
