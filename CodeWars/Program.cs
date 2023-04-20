0fb49b87a818a185e6eb6135ba61eae9de96a20d
Michael <fourjustoneday@yandex.ru>
2023-04-20 09:37:08 +0300

    61. Rotate List

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -13,31 +13,41 @@ namespace CodeWars
 {
     class Program
     {
-        public static void Rotate(int[][] matrix) {
-        
-            //write function to transpose matrix
-            for (int i = 0; i < matrix.Length; i++)
-            {
-                for (int j = i; j < matrix[i].Length; j++)
-                {
-                    (matrix[i][j], matrix[j][i]) = (matrix[j][i], matrix[i][j]);
-                }
-            }
-            
-            //write function to reverse matrix
-            for (int i = 0; i < matrix.Length; i++)
-            {
-                for (int j = 0; j < matrix[i].Length / 2; j++)
-                {
-                    (matrix[i][j], matrix[i][matrix[i].Length - 1 - j]) = (matrix[i][matrix[i].Length - 1 - j], matrix[i][j]);
-                }
+        public static ListNode RotateRight(ListNode head, int k)
+        {
 
+            if (head == null || k == 0 || head.next == null) return head;
+            ListNode fast = head;
+            ListNode slow;
+            ListNode stop = null;
+            for (int i = 0; i < k; i++)
+            {
+                fast = fast.next;
+                if (fast == null)
+                {
+                    fast = head;
+                }
             }
+
+            if (fast == head) return head;
+          
+            slow = head;
+            while (fast.next != null)
+            {
+                fast = fast.next;
+                slow = slow.next;
+            }
+            stop = slow.next;
+
+            fast.next = head;
+            slow.next = null;
+            return stop;
         }
 
         static void Main(string[] args)
         {
-            Rotate(new []{new []{1,2,3}, new []{4,5,6}, new []{7,8,9}});
+            var test = RotateRight(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))), 2);
+            var test2 = RotateRight(new ListNode(1, new ListNode(2, new ListNode(3))), 2000000000);
         }
     }
 }
