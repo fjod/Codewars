a603cd832d511b6fdfd4f921cd8fdad12994187a
Michael <fourjustoneday@yandex.ru>
2023-05-18 12:20:56 +0300

    39. Combination Sum  go

--- CodeWars/go/code/hw.go
+++ CodeWars/go/code/hw.go
@@ -4,26 +4,36 @@ import (
 	"fmt"
 )
 
-func countVowelStrings(n int) int {
+func combinationSum(candidates []int, target int) [][]int {
+	var test [][]int
+	var path []int
+	backTrack(0, 0, candidates, target, path, &test)
+	return test
+}
 
-	vowels := [5]int{1, 1, 1, 1, 1}
-	if n == 0 {
-		return 0
-	}
-	if n == 1 {
-		return 1
-	}
-	for i := 2; i <= n; i++ {
-		vowels[0] = vowels[0] + vowels[1] + vowels[2] + vowels[3] + vowels[4]
-		vowels[1] = vowels[1] + vowels[2] + vowels[3] + vowels[4]
-		vowels[2] = vowels[2] + vowels[3] + vowels[4]
-		vowels[3] = vowels[3] + vowels[4]
+func backTrack(index int, total int, candidates []int, target int, path []int, result *[][]int) {
+	if total == target {
+		tmp := make([]int, len(path))
+		copy(tmp, path)
+		*result = append(*result, tmp)
+		return
 	}
 
-	return vowels[0] + vowels[1] + vowels[2] + vowels[3] + vowels[4]
+	if total > target {
+		return
+	}
+	if index >= len(candidates) {
+		return
+	}
+	prevPath := path
+	path = append(path, candidates[index])
+	backTrack(index, total+candidates[index], candidates, target, path, result)
+	path = prevPath
+	backTrack(index+1, total, candidates, target, path, result)
 }
 
 func main() {
-	t := countVowelStrings(7)
+	candidates := []int{2, 3, 6, 7}
+	t := combinationSum(candidates, 7)
 	fmt.Println(t)
 }
