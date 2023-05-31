29e74ed1771dbf3a4264264d4d8d2691dfcacd67
Michael <fourjustoneday@yandex.ru>
2023-05-31 12:07:20 +0300

    303. Range Sum Query - Immutable

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -18,32 +18,30 @@ namespace CodeWars
     {
         public class Solution
         {
-            public bool WordPattern(string pattern, string s)
+            public class NumArray
             {
-                var splitted = s.Split(' ');
-                var dict = new Dictionary<char, string>();
-                if (splitted.Length != pattern.Length) return false;
-                for (var i = 0; i < pattern.Length; i++)
+                private int[] _nums;
+
+                public NumArray(int[] nums)
                 {
-                    var currentChar = pattern[i];
-                    var currentWord = splitted[i];
-                    if (!dict.ContainsKey(currentChar))
-                    {
-                        if (dict.ContainsValue(currentWord)) return false;
-                        dict.Add(currentChar, currentWord);
-                    }
-                    else
-                    {
-                        if (dict[currentChar] != currentWord) return false;
-                    }
+                    _nums = new int[nums.Length];
+                    _nums[0] = nums[0];
+                    for (int i = 1; i < nums.Length; ++i)
+                        _nums[i] = _nums[i - 1] + nums[i];
                 }
 
-                return true;
+                public int SumRange(int left, int right)
+                {
+                    var leftSum = _nums[right];
+                    if (left == 0) return leftSum;
+                    return leftSum - _nums[left - 1];
+                }
             }
 
             static void Main(string[] args)
             {
-                var arr = DistanceBetweenBusStops(new []{7,10,1,12,11,14,5,0}, 7 , 2);
+                var test = new NumArray(new[] {-2, 0, 3, -5, 2, -1});
+                test.SumRange(0, 2);
             }
         }
     }
--- CodeWars/Task.md
+++ CodeWars/Task.md
@@ -1,33 +1,35 @@
-﻿290. Word Pattern
+﻿303. Range Sum Query - Immutable
      Easy
-     6.3K
-     754
+     2.8K
+     1.8K
      Companies
-     Given a pattern and a string s, find if s follows the same pattern.
+     Given an integer array nums, handle multiple queries of the following type:
 
-Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
+Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+Implement the NumArray class:
 
+NumArray(int[] nums) Initializes the object with the integer array nums.
+int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
 
 
 Example 1:
 
-Input: pattern = "abba", s = "dog cat cat dog"
-Output: true
-Example 2:
+Input
+["NumArray", "sumRange", "sumRange", "sumRange"]
+[[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+Output
+[null, 1, -1, -3]
 
-Input: pattern = "abba", s = "dog cat cat fish"
-Output: false
-Example 3:
-
-Input: pattern = "aaaa", s = "dog cat cat dog"
-Output: false
+Explanation
+NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
+numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
+numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
 
 
 Constraints:
 
-1 <= pattern.length <= 300
-pattern contains only lower-case English letters.
-1 <= s.length <= 3000
-s contains only lowercase English letters and spaces ' '.
-s does not contain any leading or trailing spaces.
-All the words in s are separated by a single space.
+1 <= nums.length <= 104
+-105 <= nums[i] <= 105
+0 <= left <= right < nums.length
+At most 104 calls will be made to sumRange.
