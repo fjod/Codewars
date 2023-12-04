67874ef95f0aaaea1fd60005964d8eb66d01bd6e
Michael <fourjustoneday@yandex.ru>
2023-12-04 15:12:09 +0300

    9. Palindrome Number

--- golang/main.go
+++ golang/main.go
@@ -1,24 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
-func twoSum(nums []int, target int) [2]int {
-    dict := make(map[int]int)
-	dict[target - nums[0]] = 0
-	for i := 1; i < len(nums); i++ {
-		current := nums[i] 
-		index, ok := dict[current]
-		if (ok){
-			return [2]int{current, index}
-		}
-		dict[target - nums[i]] = i
+func Reverse(s string) string {
+    runes := []rune(s)
+    for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+        runes[i], runes[j] = runes[j], runes[i]
+    }
+    return string(runes)
+}
+
+func isPalindrome(x int) bool {
+    if (x < 0){return false;}
+	s1 := strconv.Itoa(x)
+	s2 := Reverse(s1)
+	return s1 == s2
+}
+
+
+func isPalindrome2(x int) bool {
+	if x < 0 {
+		return false
 	}
-	panic("not found")
+	r := 0
+	for z := x; z > 0; z /= 10 {
+		 tmp := z % 10
+		 r *= 10
+		 r += tmp
+
+	}
+	return r == x
 }
 
 func main() {
-	nums := []int{2,7,11,15}	
-	ret := twoSum(nums, 9)
-	fmt.Println(ret[0])
-	fmt.Println(ret[1])
+	test := isPalindrome2(123)
+	fmt.Printf("%t", test)
   }
