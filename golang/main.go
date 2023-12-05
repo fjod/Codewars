c78c5f420f4d9371720b6cdd8943502f6c68349d
Michael <fourjustoneday@yandex.ru>
2023-12-05 13:43:21 +0300

    2643. Row With Maximum Ones

--- golang/main.go
+++ golang/main.go
@@ -2,40 +2,34 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
-func Reverse(s string) string {
-    runes := []rune(s)
-    for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-        runes[i], runes[j] = runes[j], runes[i]
-    }
-    return string(runes)
-}
-
-func isPalindrome(x int) bool {
-    if (x < 0){return false;}
-	s1 := strconv.Itoa(x)
-	s2 := Reverse(s1)
-	return s1 == s2
-}
-
-
-func isPalindrome2(x int) bool {
-	if x < 0 {
-		return false
+func rowAndMaximumOnes(mat [][]int) []int {
+    prevSum := 0
+	biggestIndex := 0
+	size := len(mat)
+	for i := 0; i < size; i++ {
+		current:= len(mat[i])
+		sum := 0
+		for j:=0; j < current; j++{
+			sum += mat[i][j]
+		}
+		if (sum > prevSum){
+			prevSum = sum
+			biggestIndex = i
+		}
 	}
-	r := 0
-	for z := x; z > 0; z /= 10 {
-		 tmp := z % 10
-		 r *= 10
-		 r += tmp
 
-	}
-	return r == x
+	return []int {biggestIndex, prevSum}
 }
 
 func main() {
-	test := isPalindrome2(123)
-	fmt.Printf("%t", test)
+	a := [][]int{
+		{0, 0},
+		{1, 1},
+		{0, 0},
+	}
+	test := rowAndMaximumOnes(a)
+	fmt.Printf("%d", test[0])
+	fmt.Printf("%d", test[1])
   }
