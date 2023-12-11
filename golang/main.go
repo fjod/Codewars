2e028ef02489b410c52f0995f541d050704e0709
Michael <fourjustoneday@yandex.ru>
2023-12-11 13:58:12 +0300

    1122. Relative Sort Array

--- golang/main.go
+++ golang/main.go
@@ -2,34 +2,78 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
-func rowAndMaximumOnes(mat [][]int) []int {
-    prevSum := 0
-	biggestIndex := 0
-	size := len(mat)
-	for i := 0; i < size; i++ {
-		current:= len(mat[i])
-		sum := 0
-		for j:=0; j < current; j++{
-			sum += mat[i][j]
-		}
-		if (sum > prevSum){
-			prevSum = sum
-			biggestIndex = i
+type Occurence struct {
+	Value int
+	Amount int
+}
+
+func relativeSortArray(arr1 []int, arr2 []int) []int {
+    occurences := CalcOccurencesInArr2(arr2, arr1)
+	lefties := FindLeftArr1Values(arr1, occurences)
+	ret := []int{}
+	for i := 0; i < len(occurences); i++ {
+		current := occurences[i]
+		for i := 0; i < current.Amount; i++ {
+			ret = append(ret, current.Value)			
 		}
 	}
+	for i := 0; i < len(lefties); i++ {
+		current := lefties[i]
+		ret = append(ret, current)			
+	}
+	return ret
+}
 
-	return []int {biggestIndex, prevSum}
+func CalcOccurencesInArr2(arr2 []int, arr1[]int) []Occurence {
+	ret := []Occurence{}
+	for i := 0; i < len(arr2); i++ {
+		current := arr2[i]
+		amount := 0
+		for _, v := range arr1 {
+			if v == current {
+				amount += 1
+			}
+		}
+		ret = append(ret, Occurence{Value: current, Amount: amount})
+	}
+
+	return ret
+}
+
+func FindLeftArr1Values(arr1 []int, occ []Occurence) []int{
+	ret := []int{}
+	for i := 0; i < len(arr1); i++ {
+		current := arr1[i]	
+		foundThisOccurence := false	
+		for _, v := range occ {
+			if v.Value == current {
+				foundThisOccurence = true
+				continue
+			}
+		}
+		if !foundThisOccurence{
+			ret = append(ret,current)
+		}
+		
+		sort.Slice(ret, func(i, j int) bool {
+			return ret[i] < ret[j]
+		 })
+	}
+
+	return ret
 }
 
 func main() {
-	a := [][]int{
-		{0, 0},
-		{1, 1},
-		{0, 0},
-	}
-	test := rowAndMaximumOnes(a)
-	fmt.Printf("%d", test[0])
-	fmt.Printf("%d", test[1])
+	a1 := []int{2,3,1,3,2,4,6,7,9,2,19}
+	a2 := []int{2,1,4,3,9,6}
+	test := relativeSortArray(a1, a2)
+	fmt.Printf("%d", test)	
+
+	a1 = []int{28,6,22,8,44,17}
+	a2 = []int{22,28,8,6}
+	test = relativeSortArray(a1, a2)
+	fmt.Printf("%d", test)	
   }
