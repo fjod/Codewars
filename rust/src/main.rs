59c0e838db77c105cc600c90ac15e719716a4019
fourj <fourjustooneday@yandex.ru>
2025-01-17 15:31:42 +0300

    3110. Score of a String

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,21 +1,26 @@
 fn main() {
-    let vec_mut = vec![4,1,2,1,2];
-    let test = single_number(vec_mut);
+    let str = String::from("");
+    let test = score_of_string(str);
     println!("result is {test}")
 }
 
-pub fn single_number(nums: Vec<i32>) -> i32 {
-    let mut map = std::collections::HashMap::new();
-    for item in &nums {
-        map.entry(item)
-            .and_modify(|counter| *counter += 1)
-            .or_insert(1);
-    }
+pub fn score_of_string(s: String) -> i32 {
+    let mut score = 0;
+    let mut chars = s.chars();
 
-    for (&key, &value) in &map {
-        if value == 1 {
-            return *key;
+    //if let Some(first) = chars.next() is a concise way to handle the Option
+    // returned by chars.next() and bind the value to first if it exists.
+    if let Some(first) = chars.next() {
+        let mut prev = first as i32;
+
+        for current in chars {
+            let current_val = current as i32;
+            score += (prev - current_val).abs();
+            prev = current_val;
         }
     }
-    -1
+
+
+
+    score
 }
