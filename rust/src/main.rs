3614ff696df386734d1b6ad8f1f4f80bf6dfb46a
fourj <fourjustooneday@yandex.ru>
2025-01-19 19:54:00 +0300

    1929. Concatenation of Array

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,26 +1,20 @@
 fn main() {
-    let str = String::from("");
-    let test = score_of_string(str);
-    println!("result is {test}")
+    let vec = vec![1, 2, 3];
+    let test = get_concatenation(vec);
+    println!("All elements: {:?}", test);
 }
 
-pub fn score_of_string(s: String) -> i32 {
-    let mut score = 0;
-    let mut chars = s.chars();
-
-    //if let Some(first) = chars.next() is a concise way to handle the Option
-    // returned by chars.next() and bind the value to first if it exists.
-    if let Some(first) = chars.next() {
-        let mut prev = first as i32;
-
-        for current in chars {
-            let current_val = current as i32;
-            score += (prev - current_val).abs();
-            prev = current_val;
+pub fn get_concatenation(mut nums: Vec<i32>) -> Vec<i32> {
+  /*  let mut vec: Vec<i32> = Vec::with_capacity(nums.len() * 2);
+    for _ in 0..2 {
+        for i in 0..nums.len() {
+            vec.push(nums[i])
         }
     }
-
-
-
-    score
+    vec*/
+    let len = nums.len();
+    for i in 0..len {
+        nums.push(nums[i])
+    }
+    nums
 }
