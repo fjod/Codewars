61beedda860e0aff5f9b3be494222d590b74cd88
four <fourjustoneday@yandex.ru>
2025-02-09 18:47:58 +0300

    2894. Divisible and Non-divisible Sums Difference

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,19 +1,18 @@
 fn main() {
-    let test = get_sneaky_numbers(vec![7,1,5,4,3,4,6,0,9,5,8,2]);
-    println!("Amount of pairs {:?}", test);
+    let test = difference_of_sums(10 ,3);
+    println!("result {:?}", test);
 }
 
-pub fn get_sneaky_numbers(nums: Vec<i32>) -> Vec<i32> {
-    let mut unique_numbers: std::collections::HashSet<i32> = std::collections::HashSet::new();
-    for i in 0..nums.len() {
-        for j in i+1..nums.len(){
-            if nums[i] == nums[j]
-                { unique_numbers.insert(nums[i]); }
+pub fn difference_of_sums(n: i32, m: i32) -> i32 {
+    let mut sum1 = 0;
+    let mut sum2 = 0;
+    for i in 1..n + 1 {
+        if i % m == 0 {
+            sum2 += i;
+        }
+        else {
+            sum1 += i;
         }
     }
-    let mut sneaky_numbers: Vec<i32> = Vec::with_capacity(2);
-    for n in unique_numbers {
-        sneaky_numbers.push(n)
-    }
-    sneaky_numbers
+    sum1 - sum2
 }
