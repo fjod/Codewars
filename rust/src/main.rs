95ac576bb265e562371554035e9d0a27ddcafcc3
four <fourjustoneday@yandex.ru>
2025-02-14 15:04:53 +0300

    13. Roman to Integer

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,54 +1,56 @@
 fn main() {
-    let test = is_palindrome(9999);
+    let test = roman_to_int(String::from("MCMXCIV"));
+
     println!("result {:?}", test);
 }
 
-pub fn is_palindrome(x: i32) -> bool {
-    let mut current_x = x;
-    let ten : i32 = 10;
-    let digits = count_digits(x);
-    if (x < 0){
-        return false;
-    }
-    if (x == 0){
-        return true
-    }
-    let half = digits / 2;
-    let mut pow = ten.pow(digits - 1); // 100?
-    for _i in 0..half {
-        let fst_digit = current_x / pow;
-        let last_digit = last_digit(current_x);
-        if fst_digit != last_digit {
-            return false;
+
+pub fn roman_to_int(s: String) -> i32 {
+    let mut result : i32 = 0;
+    let mut roman_to_numeric: std::collections::HashMap<char, i32> = std::collections::HashMap::new();
+
+    // Insert key-value pairs into the HashMap
+    roman_to_numeric.insert('I', 1);
+    roman_to_numeric.insert('V', 5);
+    roman_to_numeric.insert('X', 10);
+    roman_to_numeric.insert('L', 50);
+    roman_to_numeric.insert('C', 100);
+    roman_to_numeric.insert('D', 500);
+    roman_to_numeric.insert('M', 1000);
+
+    let mut substracts: std::collections::HashMap<String, i32> = std::collections::HashMap::new();
+
+    // Insert key-value pairs into the HashMap
+    substracts.insert(String::from("IV"), 4);
+    substracts.insert(String::from("IX"), 9);
+    substracts.insert(String::from("XL"), 40);
+    substracts.insert(String::from("XC"), 90);
+    substracts.insert(String::from("CD"), 400);
+    substracts.insert(String::from("CM"), 900);
+
+    for (i, current) in s.chars().rev().enumerate() {
+        let prev = get_prev(&s, i);
+        match prev {
+            None => result += roman_to_numeric[&current],
+            Some(prev) => {
+                let two_chars = format!("{}{}", current, prev);
+                if substracts.contains_key(&two_chars) {
+                    result -= roman_to_numeric[&prev];
+                    result += substracts[&two_chars]
+                }
+                else {
+                    // предыдущий символ не подразумевает вычитания, добавляем текущий
+                    result += roman_to_numeric[&current]
+                }
+            }
         }
-        current_x = current_x - fst_digit * pow; // remove first digit
-        current_x = current_x / 10; // remove last digit
-        pow = pow / 100;
     }
-    true
+    result
 }
 
-fn count_digits(num: i32) -> u32 {
-    // Handle the case for zero separately
-    if num == 0 {
-        return 1;
+fn get_prev(s: &String, current :  usize) -> Option<char> {
+    if current == 0 {
+        return None;
     }
-
-    // Convert the number to its absolute value
-    let mut n = num.abs();
-    let mut count = 0;
-
-    // Count the digits
-    while n > 0 {
-        count += 1;
-        n /= 10;
-    }
-
-    count
-}
-
-fn last_digit(num: i32) -> i32 {
-    // Ensure the number is positive for the modulus operation
-    let abs_num = num.abs();
-    abs_num % 10
+    s.chars().rev().nth(current - 1)
 }
