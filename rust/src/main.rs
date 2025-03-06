8aaa701290c8d34ff938132ebeb15a4c54a1671b
four <fourjustoneday@yandex.ru>
2025-03-06 17:25:43 +0300

    28. Find the Index of the First Occurrence in a String

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,67 +2,30 @@ mod data;
 use data::list::ListNode; // Refers to ListNode in src/data/data.rs
 
 fn main() {
-    let mut insert = vec![3, 2, 2, 3];
-    let result = remove_element(&mut insert, 3);
-    println!("result {:?} {:?}", insert, result);
+    let str1 = String::from("sadbutsad");
+    let str2 = String::from("sad");
+    let result = str_str(str1, str2);
+    println!("result {:?}", result);
 }
 
-pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
-    let len = nums.len();
-    if len == 0 {
-        return 0;
-    }
-    let mut right = len - 1;
-    let mut counter = 0;
-
-    for i in 0..len {
-        let cur = nums[i];
-        if cur == val {
-            while (right > i && nums[right] == val) {
-                // ищем первый подходящий элемент справа
-                right = right - 1;
-            }
-            if (right <= i) {
-                // если не нашли, то выходим
-                break;
-            }
-            nums[i] = nums[right];
-            nums[right] = val;
-            right = right - 1;
+pub fn str_str(haystack: String, needle: String) -> i32 {
+        let needle_len = needle.len();
+        if needle_len == 0 {
+            return 0;
+        }
+        if needle_len > haystack.len() {
+            return -1;
         }
 
-        counter = counter + 1;
-    }
-
-    counter
-}
-
-pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
-    let len = nums.len();
-    if len <= 1 {
-        return len as i32;
-    }
-    let mut total_uniques = 1;
-    let mut place_for_next: Option<usize> = None;
-
-    for i in 0..len - 1 {
-        let cur = nums[i];
-        let next = nums[i + 1];
-        if cur == next {
-            if place_for_next == None
-            // сохранить первое место появления дубликата
-            {
-                place_for_next = Some(i + 1);
-                continue;
+        let haystack_len = haystack.len();
+        for (index, _) in haystack.chars().enumerate() {
+            if (index + needle_len) > haystack_len {
+                return -1;
             }
-        } else {
-            // если встретили новую цифру и до этого запоминали адрес первого дубликата
-            if let Some(place) = place_for_next {
-                nums[place] = next;
-                place_for_next = Some(place + 1); // запоминаем где следующее место для дубликата
-            }
-            total_uniques = total_uniques + 1;
+            let sub_hay_stack = &haystack[index..index+needle_len];
+            if sub_hay_stack == needle {
+                return index as i32;
+            }          
         }
-    }
-    total_uniques
+        return -1   
 }
