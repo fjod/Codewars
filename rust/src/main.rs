1a606ccee12ecb9d6d575948860457c10a8099af
four <fourjustoneday@yandex.ru>
2025-03-17 16:12:32 +0300

    69. Sqrt(x)

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,41 +1,34 @@
 mod data;
+
 use data::list::ListNode; // Refers to ListNode in src/data/data.rs
 
-fn main() {  //     294 / 296 testcases passed
-    let result = add_binary(
-        String::from("11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"),
-         String::from("1"));
-    println!("result {:?}", result);
+fn main() {
+    let result = my_sqrt(2147395599);
+    let sqrt = (2147395599.0f32).sqrt();
+    println!("result {:?} {:?}", result, sqrt);
 } 
 
-pub fn add_binary(a: String, b: String) -> String {
-        let s1 = convert_to_binary(&a);
-        let s2 = convert_to_binary(&b);
-        int_to_binary(s1 + s2)
-}
+pub fn my_sqrt(x: i32) -> i32 {
+        let mut left: u128 = 0;
+        let mut right: u128 = x as u128;
+        let mut mid: u128 = 0;
+        while left <= right {
+            mid = left + (right - left) / 2;
 
-fn convert_to_binary(input: &str) -> u128{
-    let mut result = 0;
-    let mut n = 0;
-    for c in input.chars().rev() {
-        if c == '1' {
-            result += 2u128.pow(n);
+            if mid * mid == (x as u128) {                
+                return mid as i32;
+            }
+            if mid * mid > (x as u128) {
+                if (mid -1 ) * (mid -1) < (x as u128) {
+                    return (mid - 1) as i32;
+                }
+            }
+
+            if mid * mid < (x as u128) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
-        n += 1;
-    }
-    result
-}
-
-fn int_to_binary(input: u128) -> String {
-    if input == 0 {
-        return "0".to_string();
-    }
-    let mut result = String::new();
-    let mut n = input;
-    while n > 0 {
-        let remainder = n % 2;
-        result.push_str(&remainder.to_string());
-        n = n / 2;
-    }
-    result.chars().rev().collect()
+        panic!("Error");
 }
