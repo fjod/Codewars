841a676ae70ee5d51b45fa2d16d91defd9fbbfb1
four <fourjustoneday@yandex.ru>
2025-03-20 15:31:32 +0300

    83. Remove Duplicates from Sorted List

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,26 +3,47 @@ mod data;
 use data::list::ListNode; // Refers to ListNode in src/data/data.rs
 
 fn main() {
-    let result = climb_stairs(5);  
+    let mut head = ListNode::new(1);
+    let next = ListNode::new(1);
+    head.next = Some(Box::new(next));
+    let next = ListNode::new(2);    
+    head.next.as_mut().unwrap().next = Some(Box::new(next));
+
+    let result = delete_duplicates(Some(Box::new(head)));  
     println!("result {:?}", result);
 } 
 
-pub fn climb_stairs(n: i32) -> i32 {
-        if n == 1 {
-            return 1;
+pub fn delete_duplicates(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    let mut current = &mut head;
+    while let Some(ref mut node) = current {
+        while let Some(ref next) = node.next {
+            if node.val == next.val {
+                node.next = next.next.clone();
+            } else {
+                break;
+            }
         }
-        if n == 2 {
-            return 2;
+        current = &mut node.next;
+    }
+    head
+}
+
+// ownership gymnastics
+pub fn delete_duplicates2(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    let mut current = &mut head;
+    while let Some(node) = current.take() {
+        let mut node = node;
+        let mut next_opt = node.next.take();
+        while let Some(mut next) = next_opt {
+            if node.val == next.val {
+                next_opt = next.next.take();
+            } else {
+                node.next = Some(next);
+                break;
+            }
         }
-        if n == 3 {
-            return 3;
-        }
-        let mut dp = vec![0; n as usize];
-        dp[0] = 1;
-        dp[1] = 2;
-        dp[2] = 3;
-        for i in 3..n as usize {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-        dp[n as usize - 1]
+        *current = Some(node);
+        current = &mut current.as_mut().unwrap().next;
+    }
+    head
 }
