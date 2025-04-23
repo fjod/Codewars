a7c7644c652edb2f84e5c27a442b53dc419bb9a3
four <fourjustoneday@yandex.ru>
2025-04-23 14:46:08 +0300

    144. Binary Tree Preorder Traversal

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -3,67 +3,124 @@ using System.Collections.Generic;
 using System.Linq;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
+using CodeWars;
 
 
 namespace CodeWars
 {
-    /* 
-        | Method             | Mean      | Error    | StdDev   | Allocated |
-        |------------------- |----------:|---------:|---------:|----------:|
-        | MajorityArraySort  |  65.18 us | 1.260 us | 1.808 us |         - |
-        | MajorityDictionary | 164.88 us | 2.510 us | 1.960 us |     424 B |
-        */
-    public class Majority
+   /*
+| Method      | Mean     | Error     | StdDev    |
+|------------ |---------:|----------:|----------:|
+| StartNonRec | 6.188 us | 0.1059 us | 0.0990 us |
+| StartRec    | 6.199 us | 0.1213 us | 0.1534 us |
+really no diff here, so I wonder why leetcode asks for non-recursive version
+    */
+    public class PreorderTraversal
     {
-        private static IEnumerable<int> Generate()
+        private static TreeNode GenerateTree()
         {
-            for (int i = 0; i < 15000; i++)
-            {
-                yield return 1;
-            }
+            var root = new TreeNode(1);
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
 
-            for (int i = 2; i < 5; i++)
+            int value = 2; // Start assigning values from 2
+            while (value <= 1000)
             {
-                for (int k = 0; k < 2000; k++)
+                var current = queue.Dequeue();
+
+                // Add left child
+                if (value <= 1000)
                 {
-                    yield return i;
+                    current.left = new TreeNode(value++);
+                    queue.Enqueue(current.left);
+                }
+
+                // Add right child
+                if (value <= 1000)
+                {
+                    current.right = new TreeNode(value++);
+                    queue.Enqueue(current.right);
                 }
             }
-        }
 
-        private readonly int[] _nums = Generate().ToArray();
+            return root;
+        }
+        [Benchmark]
+        public IList<int> StartNonRec()
+        {
+            var root = GenerateTree();
+            return PreorderTraversalNonRec(root).ToList();
+        }
+        
+        public IList<int> PreorderTraversalNonRec(TreeNode root) {
+            if (root == null)
+            {
+                return new List<int>(0);
+            }
+            IList<int> result = new List<int>();
+            Order(root, result);
+            return result;
+        }
         
         [Benchmark]
-        public int MajorityArraySort() {
-            Array.Sort(_nums);
-            return _nums[_nums.Length / 2];
+        public IList<int> StartRec()
+        {
+            var root = GenerateTree();
+            return PreorderTraversalRec(root).ToList();
         }
         
-        [Benchmark]
-        public int MajorityDictionary() {
+       
+        public IList<int> PreorderTraversalRec(TreeNode root) {
+            if (root == null)
+            {
+                return new List<int>(0);
+            }
+            IList<int> result = new List<int>();
+            OrderRec(root, result);
+            return result;
+        }
+
+        void Order(TreeNode node, IList<int> result)
+        {
+            while (true)
+            {
+                if (node == null)
+                {
+                    return;
+                }
+
+                result.Add(node.val);
+                if (node.left != null)
+                {
+                    Order(node.left, result);
+                }
+
+                // we are at most depth for left side here, so time to add rights and then return back up
+                if (node.right != null)
+                {
+                    node = node.right;
+                    continue;
+                }
+
+                break;
+            }
+        }
         
-            // can work without the array
-            Dictionary<int, int> dict = new Dictionary<int, int>();
-            int counter = 0;
-            foreach (var num in Generate())
+        void OrderRec(TreeNode node, IList<int> result)
+        {
+            if (node == null)
             {
-                if (!dict.TryAdd(num, 1))
-                {
-                    dict[num]++;
-                }
-
-                counter++;
+                return;
             }
-
-            foreach (var num in dict)
+            result.Add(node.val);
+            if (node.left != null)
             {
-                if (num.Value > counter / 2)
-                {
-                    return num.Key;
-                }
+                Order(node.left, result);
             }
-
-            throw new Exception();
+            if (node.right != null)
+            {
+                Order(node.right, result);
+            }
         }
     }
     
@@ -72,7 +129,7 @@ namespace CodeWars
        
         static void Main(string[] args)
         {
-            BenchmarkRunner.Run<Majority>();
+            BenchmarkRunner.Run<PreorderTraversal>();
         }
     }
 }
