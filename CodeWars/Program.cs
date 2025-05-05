7b5f6b7f686adfcee171c2b9342a2c6c9ff93ae5
four <fourjustoneday@yandex.ru>
2025-05-05 12:51:17 +0300

    203. Remove Linked List Elements

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -1,135 +1,104 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
-using CodeWars;
 
 
-namespace CodeWars
+namespace CodeWars;
+
+/*
+| Method    | Mean     | Error     | StdDev    |
+|---------- |---------:|----------:|----------:|
+| StartMine | 3.307 us | 0.0232 us | 0.0193 us |
+| StartGrok | 3.255 us | 0.0628 us | 0.0617 us | 
+ */
+public class RemoveLinkedListElement
 {
-   /*
-| Method      | Mean     | Error     | StdDev    |
-|------------ |---------:|----------:|----------:|
-| StartNonRec | 6.188 us | 0.1059 us | 0.0990 us |
-| StartRec    | 6.199 us | 0.1213 us | 0.1534 us |
-really no diff here, so I wonder why leetcode asks for non-recursive version
-    */
-    public class PreorderTraversal
+    private ListNode ListNodeGenerate()
     {
-        private static TreeNode GenerateTree()
+        Random random = new Random();
+        var head = new ListNode(1);
+        var current = head;
+        for (int i = 2; i <= 1000; i++)
         {
-            var root = new TreeNode(1);
-            var queue = new Queue<TreeNode>();
-            queue.Enqueue(root);
-
-            int value = 2; // Start assigning values from 2
-            while (value <= 1000)
-            {
-                var current = queue.Dequeue();
-
-                // Add left child
-                if (value <= 1000)
-                {
-                    current.left = new TreeNode(value++);
-                    queue.Enqueue(current.left);
-                }
-
-                // Add right child
-                if (value <= 1000)
-                {
-                    current.right = new TreeNode(value++);
-                    queue.Enqueue(current.right);
-                }
-            }
-
-            return root;
+            current.next = new ListNode(random.Next(0, 100));
+            current = current.next;
         }
-        [Benchmark]
-        public IList<int> StartNonRec()
-        {
-            var root = GenerateTree();
-            return PreorderTraversalNonRec(root).ToList();
-        }
-        
-        public IList<int> PreorderTraversalNonRec(TreeNode root) {
-            if (root == null)
-            {
-                return new List<int>(0);
-            }
-            IList<int> result = new List<int>();
-            Order(root, result);
-            return result;
-        }
-        
-        [Benchmark]
-        public IList<int> StartRec()
-        {
-            var root = GenerateTree();
-            return PreorderTraversalRec(root).ToList();
-        }
-        
-       
-        public IList<int> PreorderTraversalRec(TreeNode root) {
-            if (root == null)
-            {
-                return new List<int>(0);
-            }
-            IList<int> result = new List<int>();
-            OrderRec(root, result);
-            return result;
-        }
-
-        void Order(TreeNode node, IList<int> result)
-        {
-            while (true)
-            {
-                if (node == null)
-                {
-                    return;
-                }
-
-                result.Add(node.val);
-                if (node.left != null)
-                {
-                    Order(node.left, result);
-                }
-
-                // we are at most depth for left side here, so time to add rights and then return back up
-                if (node.right != null)
-                {
-                    node = node.right;
-                    continue;
-                }
-
-                break;
-            }
-        }
-        
-        void OrderRec(TreeNode node, IList<int> result)
-        {
-            if (node == null)
-            {
-                return;
-            }
-            result.Add(node.val);
-            if (node.left != null)
-            {
-                Order(node.left, result);
-            }
-            if (node.right != null)
-            {
-                Order(node.right, result);
-            }
-        }
+        return head;
     }
     
-    class Program
+    [Benchmark]
+    public ListNode StartMine()
     {
-       
-        static void Main(string[] args)
-        {
-            BenchmarkRunner.Run<PreorderTraversal>();
+        var root = ListNodeGenerate();
+        return RemoveElementsMine(root, 7);
+    }
+    
+    [Benchmark]
+    public ListNode StartGrok()
+    {
+        var root = ListNodeGenerate();
+        return RemoveElementsGrok(root, 7);
+    }
+    
+    public ListNode RemoveElementsGrok(ListNode head, int val) {
+        // Create a dummy node to handle cases where head needs to be removed
+        ListNode dummy = new ListNode(0, head);
+        ListNode current = dummy;
+
+        // Traverse the list
+        while (current.next != null) {
+            if (current.next.val == val) {
+                // Skip the node with the target value
+                current.next = current.next.next;
+            } else {
+                // Move to the next node
+                current = current.next;
+            }
         }
+
+        return dummy.next;
+    }
+    
+    public ListNode RemoveElementsMine(ListNode head, int val) {
+        if (head == null)
+            return head;
+
+        // skip all elements at the start
+        if (head.val == val)
+        {
+            while (head != null && head.val == val)
+            {
+                head = head.next;
+            }
+
+            // all elements are removed?
+            if (head == null)
+            {
+                return null;
+            }
+        }
+        
+        var current = head;
+        while (current.next != null)
+        {
+            if (current.next.val == val)
+            {
+                current.next = current.next.next;
+            }
+            else
+            {
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<RemoveLinkedListElement>();
     }
 }
