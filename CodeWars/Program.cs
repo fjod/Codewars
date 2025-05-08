70c510d927639206a7d919e7f7da77fedc9da9e8
four <fourjustoneday@yandex.ru>
2025-05-08 10:40:56 +0300

    spans benches

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -1,97 +1,221 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 
 
 namespace CodeWars;
 
-/*
-| Method    | Mean     | Error     | StdDev    |
-|---------- |---------:|----------:|----------:|
-| StartMine | 3.307 us | 0.0232 us | 0.0193 us |
-| StartGrok | 3.255 us | 0.0628 us | 0.0617 us | 
- */
-public class RemoveLinkedListElement
+public class Sample
 {
-    private ListNode ListNodeGenerate()
+    public string S1 { get; set; }
+    public string S2 { get; set; }
+    public string S3 { get; set; }
+    public string S4 { get; set; }
+    public string S5 { get; set; }
+}
+
+public class Samples
+{
+    public List<Sample> Type { get; set; }
+}
+
+/*
+| Method                   | Mean     | Error    | StdDev   | Gen0    | Gen1    | Gen2    | Allocated |
+|------------------------- |---------:|---------:|---------:|--------:|--------:|--------:|----------:|
+| SquashSamplesNoSbSpans   | 35.11 us | 0.704 us | 2.066 us | 47.6074 | 47.6074 | 47.6074 |  150.7 KB | -- if there is no need to create new string in the end, allocations = 0
+| SquashSamplesSpans       | 48.80 us | 0.556 us | 0.464 us | 45.4102 | 45.4102 | 45.4102 | 296.66 KB |
+| SquashSamplesSpansParams | 47.16 us | 0.528 us | 0.494 us | 45.4102 | 45.4102 | 45.4102 | 289.71 KB |
+| SquashSamplesString      | 48.71 us | 0.694 us | 0.649 us | 47.6074 | 47.6074 | 47.6074 | 449.71 KB |
+
+
+ */
+[MemoryDiagnoser]
+public class Spans
+{
+    private static readonly List<Sample> Start = GenerateSamples(50);
+    
+    public static string GenerateRandomString()
     {
-        Random random = new Random();
-        var head = new ListNode(1);
-        var current = head;
-        for (int i = 2; i <= 1000; i++)
+        Random random = Random.Shared;
+        int length = random.Next(100, 500);
+        Span<char> chars = stackalloc char[length];
+        for (int i = 0; i < length; i++)
         {
-            current.next = new ListNode(random.Next(0, 100));
-            current = current.next;
+            chars[i] = (char)random.Next(97, 123); // Generate lowercase letters
         }
-        return head;
+
+        return new string(chars);
+    }
+    
+    static List<Sample> GenerateSamples(int count)
+    {
+        List<Sample> samples = new List<Sample>();
+        for (int i = 0; i < count; i++)
+        {
+            samples.Add(new Sample
+            {
+                S1 = GenerateRandomString(),
+                S2 = GenerateRandomString(),
+                S3 = GenerateRandomString(),
+                S4 = GenerateRandomString(),
+                S5 = GenerateRandomString()
+            });
+        }
+        return samples;
     }
     
     [Benchmark]
-    public ListNode StartMine()
+    public void SquashSamplesNoSbSpans()
     {
-        var root = ListNodeGenerate();
-        return RemoveElementsMine(root, 7);
+        var comma = ",".AsSpan();
+        int totalLen = 0;
+        for (int i = 0; i < Start.Count; i++)
+        {
+            var cur = Start[i];
+            
+            var s1 = cur.S1;
+            var s2 = cur.S2;
+            var s3 = cur.S3;
+            var s4 = cur.S4;
+            var s5 = cur.S5;
+            totalLen += s1.Length + s2.Length + s3.Length + s4.Length + s5.Length + (comma.Length * 4);
+        }
+        // if len > 500 kb use heap
+        Span<char> result = totalLen > 524288 ? new char[totalLen] : stackalloc char[totalLen];
+        var offset = 0;
+        for (int i = 0; i < Start.Count; i++)
+        {
+            var cur = Start[i];
+            
+            var s1 = cur.S1.AsSpan();
+            var s2 = cur.S2.AsSpan();
+            var s3 = cur.S3.AsSpan();
+            var s4 = cur.S4.AsSpan();
+            var s5 = cur.S5.AsSpan();
+           
+            s1.CopyTo(result.Slice(offset, s1.Length));
+            offset += s1.Length;
+            comma.CopyTo(result.Slice(offset, comma.Length));
+            offset += comma.Length;
+            s2.CopyTo(result.Slice(offset, s2.Length));
+            offset += s2.Length;
+            comma.CopyTo(result.Slice(offset, comma.Length));
+            offset += comma.Length;
+            s3.CopyTo(result.Slice(offset, s3.Length));
+            offset += s3.Length;
+            comma.CopyTo(result.Slice(offset, comma.Length));
+            offset += comma.Length;
+            s4.CopyTo(result.Slice(offset, s4.Length));
+            offset += s4.Length;
+            comma.CopyTo(result.Slice(offset, comma.Length));
+            offset += comma.Length;
+            s5.CopyTo(result.Slice(offset, s5.Length));
+        }
+    
+        _ = new string(result[..totalLen]);
+    }
+
+    [Benchmark]
+    public void SquashSamplesSpans()
+    {
+        var comma = ",".AsSpan();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < Start.Count; i++)
+        {
+            var cur = Start[i];
+            
+            var s1 = cur.S1.AsSpan();
+            var s2 = cur.S2.AsSpan();
+            var s3 = cur.S3.AsSpan();
+            var s4 = cur.S4.AsSpan();
+            var s5 = cur.S5.AsSpan();
+            var totalLen = s1.Length + s2.Length + s3.Length + s4.Length + s5.Length + (comma.Length * 4);
+            Span<char> result = stackalloc char[totalLen];
+            var offset = 0;
+            s1.CopyTo(result.Slice(offset, s1.Length));
+            offset += s1.Length;
+            comma.CopyTo(result.Slice(offset, comma.Length));
+            offset += comma.Length;
+            s2.CopyTo(result.Slice(offset, s2.Length));
+            offset += s2.Length;
+            comma.CopyTo(result.Slice(offset, comma.Length));
+            offset += comma.Length;
+            s3.CopyTo(result.Slice(offset, s3.Length));
+            offset += s3.Length;
+            comma.CopyTo(result.Slice(offset, comma.Length));
+            offset += comma.Length;
+            s4.CopyTo(result.Slice(offset, s4.Length));
+            offset += s4.Length;
+            comma.CopyTo(result.Slice(offset, comma.Length));
+            offset += comma.Length;
+            s5.CopyTo(result.Slice(offset, s5.Length));
+            sb.Append(result);
+        }
+
+        _ = sb.ToString();
     }
     
     [Benchmark]
-    public ListNode StartGrok()
+    public void SquashSamplesSpansParams()
     {
-        var root = ListNodeGenerate();
-        return RemoveElementsGrok(root, 7);
+        var comma = ",".AsSpan();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < Start.Count; i++)
+        {
+            var cur = Start[i];
+            ConcatSpans(comma, sb, cur.S1, cur.S2, cur.S3, cur.S4, cur.S5);
+        }
+
+        _ = sb.ToString();
     }
     
-    public ListNode RemoveElementsGrok(ListNode head, int val) {
-        // Create a dummy node to handle cases where head needs to be removed
-        ListNode dummy = new ListNode(0, head);
-        ListNode current = dummy;
-
-        // Traverse the list
-        while (current.next != null) {
-            if (current.next.val == val) {
-                // Skip the node with the target value
-                current.next = current.next.next;
-            } else {
-                // Move to the next node
-                current = current.next;
+    // Helper method to concatenate spans with a separator
+    private static void ConcatSpans(ReadOnlySpan<char> separator, StringBuilder sb, params ReadOnlySpan<string> spans)
+    {
+        // Calculate total length: sum of span lengths + separator lengths
+        int totalLen = 0;
+        for (int i = 0; i < spans.Length; i++)
+        {
+            totalLen += spans[i].Length;
+            if (i < spans.Length - 1) // Separator between spans (not after last)
+            {
+                totalLen += separator.Length;
             }
         }
 
-        return dummy.next;
+        // Allocate result buffer on stack
+        Span<char> result = stackalloc char[totalLen];
+        int offset = 0;
+
+        // Copy each span and separator into result
+        for (int i = 0; i < spans.Length; i++)
+        {
+            spans[i].CopyTo(result.Slice(offset, spans[i].Length));
+            offset += spans[i].Length;
+            if (i < spans.Length - 1) // Add separator between spans
+            {
+                separator.CopyTo(result.Slice(offset, separator.Length));
+                offset += separator.Length;
+            }
+        }
+
+        sb.Append(result);
     }
     
-    public ListNode RemoveElementsMine(ListNode head, int val) {
-        if (head == null)
-            return head;
-
-        // skip all elements at the start
-        if (head.val == val)
+    [Benchmark]
+    public void SquashSamplesString()
+    {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < Start.Count; i++)
         {
-            while (head != null && head.val == val)
-            {
-                head = head.next;
-            }
-
-            // all elements are removed?
-            if (head == null)
-            {
-                return null;
-            }
-        }
-        
-        var current = head;
-        while (current.next != null)
-        {
-            if (current.next.val == val)
-            {
-                current.next = current.next.next;
-            }
-            else
-            {
-                current = current.next;
-            }
+            var cur = Start[i];
+            var result = string.Join(',', cur.S1, cur.S2, cur.S3, cur.S4, cur.S5);
+            sb.Append(result);
         }
 
-        return head;
+        _ = sb.ToString();
     }
 }
 
@@ -99,6 +223,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        BenchmarkRunner.Run<RemoveLinkedListElement>();
+        BenchmarkRunner.Run<Spans>();
     }
 }
