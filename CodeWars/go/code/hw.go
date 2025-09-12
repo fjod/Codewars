889835b1b5590b7667d1d87e2a2e29334b772602
Michael <fourjustoneday@yandex.ru>
2025-09-12 12:50:47 +0300

    292. Nim Game

--- CodeWars/go/code/hw.go
+++ CodeWars/go/code/hw.go
@@ -1,16 +1,18 @@
 package main
 
-import "sort"
-
-func missingNumber(nums []int) int {
-
-	sort.Ints(nums)
-	for i := 0; i < len(nums); i++ {
-		if nums[i] != i {
-			return i
+func canWinNim(n int) bool {
+	if n == 4 {
+		return false
+	}
+	if n == 1 || n == 2 || n == 3 {
+		return true
+	}
+	if n > 4 {
+		if n%4 == 0 {
+			return false
 		}
 	}
-	return len(nums)
+	return true
 }
 
 func main() {
