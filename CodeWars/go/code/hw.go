1b95331dca499235d1f0dcc731e54c6835944b2b
Michael <fourjustoneday@yandex.ru>
2026-03-20 09:56:06 +0300

    Interfaces & nil interface trap

--- CodeWars/go/code/hw.go
+++ CodeWars/go/code/hw.go
@@ -2,51 +2,59 @@ package main
 
 import "fmt"
 
-func append_to_slice(slice []int, value int, times int) []int {
-	for i := 0; i < times; i++ {
-		slice = append(slice, value)
-	}
-	return slice
+type MyError struct {
+	msg string
 }
 
-func bad_append(slice []int, val int, times int) {
-	for i := 0; i < times; i++ {
-		slice = append(slice, val) // realloc happens inside for a copy, caller sees nothing
-	}
+func (m MyError) Error() string {
+	return m.msg
 }
 
-func pointer_append(slice *[]int, val int, times int) {
-	for i := 0; i < times; i++ {
-		*slice = append(*slice, val) // realloc happens inside for a copy, caller sees nothing
+func ReturnNotNil() error {
+	var p *MyError = nil
+	return p
+}
+
+func ReturnNotNilCorrect() error {
+	var p *MyError = nil
+	if p == nil {
+		return nil // return untyped nil, not the typed pointer
 	}
+	return p
+}
+
+func ReturnNil() *MyError {
+	var p *MyError = nil
+	return p
+}
+
+type CustomString struct {
+	str string
+}
+
+func (c CustomString) String() string {
+	return c.str
 }
 
 func main() {
+	var isNil = ReturnNotNil() == nil
+	fmt.Printf("The code is %v\n", isNil)
 
-	slice := make([]int, 5, 8)
-	fmt.Println("before", slice)
-	slice2 := append_to_slice(slice, 99, 2)
-	fmt.Println("after, starting slice", slice)
-	fmt.Println("after, returned slice", slice2)
+	isNil = ReturnNotNilCorrect() == nil
+	fmt.Printf("The code is %v\n", isNil)
 
-	slice3 := append_to_slice(slice, 199, 5)
-	fmt.Println("after2, starting slice", slice)
-	fmt.Println("after2, starting slice2", slice2)
-	fmt.Println("after2, returned slice3", slice3)
+	isNil = ReturnNil() == nil
+	fmt.Printf("The code is %v\n", isNil)
 
-	slice_realloc := make([]int, 5, 6)
-	fmt.Println("before", slice_realloc)
-	slice_realloc_2 := append_to_slice(slice_realloc, 9, 10)
-	fmt.Println("after, starting slice", slice_realloc)
-	fmt.Println("after, returned slice", slice_realloc_2)
+	var customString = CustomString{"hello"}
+	fmt.Println(customString)
+	/*
+	 // Simplified version of what fmt does internally
+	  if s, ok := v.(fmt.Stringer); ok {
+	      output = s.String()  // uses YOUR method
+	  } else {
+	      output = reflect.TypeOf(v)...  // ugly default
+	  }
+	*/
 
-	slice_bad_realloc := make([]int, 5, 6)
-	fmt.Println("before", slice_bad_realloc)
-	bad_append(slice_bad_realloc, 8, 10) // struct is copied inside
-	fmt.Println("after, starting slice", slice_bad_realloc)
-
-	slice_good_realloc := make([]int, 5, 6)
-	fmt.Println("before", slice_good_realloc)
-	pointer_append(&slice_good_realloc, 8, 10) // using pointer, so reallocated slice is seen outside
-	fmt.Println("after, starting slice", slice_good_realloc)
 }
