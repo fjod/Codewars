da742cf2dc33bb769b4e2f38fcf1e4a9ecd966bd
Michael <fourjustoneday@yandex.ru>
2026-03-31 11:59:28 +0300

    20. Valid Parentheses

--- CodeWars/go/code/hw.go
+++ CodeWars/go/code/hw.go
@@ -1,32 +1,42 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
+import "sync"
 
-var sharedMap = make(map[string]string)
-var mutex = sync.Mutex{}
+var s sync.Once
 
 func main() {
-	var wg sync.WaitGroup
 
-	for i := 0; i < 10; i++ {
-		wg.Add(2)
-		go func(n int) {
-			defer wg.Done()
-			mutex.Lock() // run with go run -race main.go  in WSL and without mutex
-			sharedMap[fmt.Sprintf("key%d", n)] = "value"
-			mutex.Unlock()
-		}(i)
-		go func(n int) {
-			defer wg.Done()
-			mutex.Lock()
-			_ = sharedMap[fmt.Sprintf("key%d", n)]
-			mutex.Unlock()
-		}(i)
+	println(isValid("(])"))
+}
+
+func isValid(s string) bool {
+	brackets := make([]byte, 0)
+	if len(s) == 0 {
+		return true
+	}
+	if len(s) == 1 {
+		return false
 	}
 
-	wg.Wait()
-	fmt.Println("done, map length:", len(sharedMap))
+	brackets = append(brackets, s[0]) // add first
+	for i := 1; i < len(s); i++ {
+		cur := s[i]
+		if cur == '(' || cur == '[' || cur == '{' { // add any open bracket
+			brackets = append(brackets, cur)
+			continue
+		}
+		if len(brackets) == 0 { // closing bracket on empty array
+			return false
+		}
+		prev := brackets[len(brackets)-1]
+		if (cur == ')' && prev == '(') || (cur == '}' && prev == '{') || (cur == ']' && prev == '[') {
+			// remove prev bracket from slice
+			brackets = brackets[:len(brackets)-1]
+			continue
+		}
+
+		return false // prev bracket cant be removed, so we are stuck with wrong pair forever
+	}
+
+	return len(brackets) == 0
 }
